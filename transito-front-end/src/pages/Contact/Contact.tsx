46fce8b907024507267479061b26f3521ebf5cfa
ackindev <ackinlino123@gmail.com>
2024-10-29 13:45:02 -0300

    add: Reload Spinner

--- transito-front-end/src/pages/Contact/Contact.tsx
+++ transito-front-end/src/pages/Contact/Contact.tsx
@@ -1,19 +1,77 @@
 import styles from "./style.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEraser, faMailBulk } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+interface TextFormFields {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
 
 export const Contact: React.FC = () => {
+  const [formFields, setFormFields] = useState<TextFormFields>({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormFields((prevFields) => ({
+      ...prevFields,
+      [name]: value,
+    }));
+  };
+
+  const handleClear = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault(); // Previne o comportamento padrão do botão
+    setFormFields({
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    });
+  };
+
   return (
     <div className={styles.contact_form}>
       <h2>Entre em contato conosco preenchendo este formulário.</h2>
       <form>
         <div className={styles.input_container}>
-          <input type="text" placeholder="Digite seu nome..." />
-          <input type="email" placeholder="Digite seu e-mail..." />
-          <input type="tel" placeholder="Número do telefone..." />
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome..."
+            value={formFields.name}
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite seu e-mail..."
+            value={formFields.email}
+            onChange={handleChange}
+          />
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Número do telefone..."
+            value={formFields.phone}
+            onChange={handleChange}
+          />
         </div>
-        <textarea placeholder="Mensagem..."></textarea>
-        <button id={styles.clear_messages}>
+        <textarea
+          name="message"
+          placeholder="Mensagem..."
+          value={formFields.message}
+          onChange={handleChange}
+        ></textarea>
+        <button id={styles.clear_messages} onClick={handleClear}>
           Limpar <FontAwesomeIcon icon={faEraser} />
         </button>
         <button type="submit">
--- transito-front-end/src/pages/Services/Services.tsx
+++ transito-front-end/src/pages/Services/Services.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -9,10 +10,22 @@ import {
   faLocationDot,
   faBicycle,
 } from "@fortawesome/free-solid-svg-icons";
-import interdicaoImg from "../../assets/image/interdicao.png";
+import { TailSpin } from "react-loader-spinner";
 import lombadaImg from "../../assets/image/lombada.png";
 
 export const Services: React.FC = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      // Simula um carregamento de dados
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
+
   return (
     <div id={styles.title_services_info}>
       <h1>Serviços Online</h1>
@@ -21,76 +34,86 @@ export const Services: React.FC = () => {
         necessidade.
       </p>
       <div className={styles.service_options}>
-        <ul>
-          <li>
-            <a href="#">
-              <div className={styles.icon_service}>
-                <FontAwesomeIcon icon={faCar} />
-              </div>
-            </a>
-            <span>multas</span>
-          </li>
-          <li>
-            <a href="">
-              <div className={styles.icon_service}>
-                <FontAwesomeIcon icon={faWheelchair} />
-              </div>
-            </a>
-
-            <span>vagas especiais</span>
-          </li>
-          <li>
-            <a href="">
-              <div className={styles.icon_service}>
-                <FontAwesomeIcon icon={faBan} />
-              </div>
-            </a>
-            <span>sinalizações</span>
-          </li>
-          <li>
-            <a href="">
-              <div className={styles.icon_service}>
-                <FontAwesomeIcon icon={faRoadBarrier} />
-              </div>
-            </a>
-            <span>interdições</span>
-          </li>
-          <li>
-            <a href="">
-              <div className={styles.icon_service}>
-                <FontAwesomeIcon icon={faTrafficLight} />
-              </div>
-            </a>
-
-            <span>semáforos</span>
-          </li>
-          <li>
-            <a href="">
-              <div className={styles.icon_service}>
-                <img src={lombadaImg} alt="lombadas" />
-              </div>
-            </a>
-            <span>lombadas</span>
-          </li>
-          <div className={styles.center_wrapper}>
-            <li>
-              <a href="">
-                <div className={styles.icon_service}>
-                  <FontAwesomeIcon icon={faLocationDot} />
-                </div>
-              </a>
-              <span>zona-azul</span>
-            </li>
-            <li>
-              <a href="">
-                <div className={styles.icon_service}>
-                  <FontAwesomeIcon icon={faBicycle} />
-                </div>
-              </a>
-              <span>bicicletas</span>
-            </li>
+        {loading ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "20px",
+            }}
+          >
+            <TailSpin height={80} width={80} color="black" ariaLabel="loading" />
           </div>
-        </ul>
+        ) : (
+          <ul>
+            <li>
+              <a href="#">
+                <div className={styles.icon_service}>
+                  <FontAwesomeIcon icon={faCar} />
+                </div>
+              </a>
+              <span>multas</span>
+            </li>
+            <li>
+              <a href="">
+                <div className={styles.icon_service}>
+                  <FontAwesomeIcon icon={faWheelchair} />
+                </div>
+              </a>
+              <span>vagas especiais</span>
+            </li>
+            <li>
+              <a href="">
+                <div className={styles.icon_service}>
+                  <FontAwesomeIcon icon={faBan} />
+                </div>
+              </a>
+              <span>sinalizações</span>
+            </li>
+            <li>
+              <a href="">
+                <div className={styles.icon_service}>
+                  <FontAwesomeIcon icon={faRoadBarrier} />
+                </div>
+              </a>
+              <span>interdições</span>
+            </li>
+            <li>
+              <a href="">
+                <div className={styles.icon_service}>
+                  <FontAwesomeIcon icon={faTrafficLight} />
+                </div>
+              </a>
+              <span>semáforos</span>
+            </li>
+            <li>
+              <a href="">
+                <div className={styles.icon_service}>
+                  <img src={lombadaImg} alt="lombadas" />
+                </div>
+              </a>
+              <span>lombadas</span>
+            </li>
+            <div className={styles.center_wrapper}>
+              <li>
+                <a href="">
+                  <div className={styles.icon_service}>
+                    <FontAwesomeIcon icon={faLocationDot} />
+                  </div>
+                </a>
+                <span>zona-azul</span>
+              </li>
+              <li>
+                <a href="">
+                  <div className={styles.icon_service}>
+                    <FontAwesomeIcon icon={faBicycle} />
+                  </div>
+                </a>
+                <span>bicicletas</span>
+              </li>
+            </div>
+          </ul>
+        )}
       </div>
     </div>
   );
