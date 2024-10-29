285c4448f687d26702d7b5798a8563e26d56a6ee
ackindev <ackinlino123@gmail.com>
2024-10-29 09:05:02 -0300

    Add: Formulário para entrar em contato

--- transito-front-end/src/pages/Contact/Contact.tsx
+++ transito-front-end/src/pages/Contact/Contact.tsx
@@ -1,7 +1,25 @@
+import styles from "./style.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEraser, faMailBulk } from "@fortawesome/free-solid-svg-icons";
+
 export const Contact: React.FC = () => {
   return (
-    <div>
-      <p>Contato</p>
+    <div className={styles.contact_form}>
+      <h2>Entre em contato conosco preenchendo este formulário.</h2>
+      <form>
+        <div className={styles.input_container}>
+          <input type="text" placeholder="Digite seu nome..." />
+          <input type="email" placeholder="Digite seu e-mail..." />
+          <input type="tel" placeholder="Número do telefone..." />
+        </div>
+        <textarea placeholder="Mensagem..."></textarea>
+        <button id={styles.clear_messages}>
+          Limpar <FontAwesomeIcon icon={faEraser} />
+        </button>
+        <button type="submit">
+          Enviar <FontAwesomeIcon icon={faMailBulk} />
+        </button>
+      </form>
     </div>
   );
 };
