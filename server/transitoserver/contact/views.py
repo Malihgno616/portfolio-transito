4bb152394aac22980a43309d1eba367ec924faf4
ackindev <ackinlino123@gmail.com>
2024-11-22 08:39:59 -0300

    Removendo as pastas /__pycache__/

--- server/transitoserver/contact/views.py
+++ server/transitoserver/contact/views.py
@@ -6,6 +6,8 @@ from .serializers import ContactFormSerializer
 from django.core.mail import BadHeaderError, send_mail
 from django.conf import settings
 import logging
+from django.http import Http404
+import logging
 
 # Configuração do logger para depuração
 logger = logging.getLogger(__name__)
@@ -13,13 +15,7 @@ logger = logging.getLogger(__name__)
 class ContactSubmitView(APIView):
     def post(self, request):
         data = request.data
-        required_fields = ['name', 'email', 'phone', 'message']
-        
-        # Verificar se os campos obrigatórios estão presentes
-        for field in required_fields:
-            if field not in data:
-                logger.error("Campo ausente: %s", field)
-                return Response({'error': f'Campo ausente: {field}'}, status=status.HTTP_400_BAD_REQUEST)
+        required_fields = ['name', 'email', 'phone', 'message']      
 
         # Salvar os dados no banco de dados
         try:
@@ -53,15 +49,11 @@ class ContactSubmitView(APIView):
             # Enviar o e-mail
             send_mail(subject, message, from_email, to_email)
             logger.info("E-mail enviado com sucesso para %s", to_email)
-        except BadHeaderError:
-            logger.error("Cabeçalho inválido no e-mail.")
-            return Response({'error': 'Cabeçalho inválido no e-mail'}, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             logger.error("Erro ao enviar o e-mail: %s", str(e))
             return Response({'error': f'Erro ao enviar o e-mail: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         return Response({'message': 'Mensagem recebida e enviada por e-mail com sucesso!'}, status=status.HTTP_201_CREATED)
-
 
 class ContactListView(APIView):
     def get(self, request):
@@ -69,17 +61,6 @@ class ContactListView(APIView):
         serializer = ContactFormSerializer(contacts, many=True)
 
         return Response(serializer.data)
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from .models import ContactForm
-from .serializers import ContactFormSerializer
-from django.http import Http404
-import logging
-
-# Configuração do logger para depuração
-logger = logging.getLogger(__name__)
 
 class ContactDeleteView(APIView):
     def delete(self, request, pk):
