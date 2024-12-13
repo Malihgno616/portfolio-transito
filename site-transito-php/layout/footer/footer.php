0f77e7bd3d0df073aef4114f657211cc97bf9c03
ackindev <ackinlino123@gmail.com>
2024-12-13 13:57:50 -0300

    Add footer, falta terminar

--- site-transito-php/layout/footer/footer.php
+++ site-transito-php/layout/footer/footer.php
@@ -1,6 +1,13 @@
 <footer>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur doloremque ad dolor esse beatae temporibus magni non labore officia. Quaerat doloremque quia illo unde sapiente vitae quasi ex aspernatur nobis.</p>
+  <div>
+    <img src="../assets/img/favicon.png" alt="Logo Leme" width="100px">
+  </div>
+  <h1><em>Coordenadoria Municipal de Trânsito</em></h1>
+  <h2><em>Secretaria de Segurança, Trânsito, Cidadania e Defesa Civil</em></h2>
+  <ul>
+    <li>Endereço</li>
+    <li>Telefone</li>
+    <li>Email</li>
+    <li>Atendimento</li>
+  </ul>
 </footer>
-
-</body>
-</html>
--- site-transito-php/public/index.php
+++ site-transito-php/public/index.php
@@ -1,4 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Transito Leme</title>
+  <link rel="stylesheet" href="../assets/css/header.css">
+  <link rel="stylesheet" href="../assets/css/global.css">
+  <link rel="stylesheet" href="../assets/css/footer.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+<body>
+  <?php include_once('../layout/header/header.php');?>  
+  <?php include_once('../layout/slider/slider.php');?>
+  <?php include_once('../layout/footer/footer.php');?>
 
 
 
 
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
