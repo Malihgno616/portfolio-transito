788836e2f8aaabcfdf2001e9affc76899883e189
ackindev <ackinlino123@gmail.com>
2024-12-13 13:57:58 -0300

    Ainda não

--- site-transito-php/layout/footer/footer.php
+++ site-transito-php/layout/footer/footer.php
@@ -1,13 +1,15 @@
 <footer>
   <div>
     <img src="../assets/img/favicon.png" alt="Logo Leme" width="100px">
+    <h1><em>Coordenadoria Municipal de Trânsito</em></h1>
+    <h2><em>Secretaria de Segurança, Trânsito, Cidadania e Defesa Civil</em></h2>
   </div>
-  <h1><em>Coordenadoria Municipal de Trânsito</em></h1>
-  <h2><em>Secretaria de Segurança, Trânsito, Cidadania e Defesa Civil</em></h2>
-  <ul>
-    <li>Endereço</li>
-    <li>Telefone</li>
-    <li>Email</li>
-    <li>Atendimento</li>
-  </ul>
+  <div>
+    <ul>
+      <li>Endereço</li>
+      <li>Telefone</li>
+      <li>Email</li>
+      <li>Atendimento</li>
+    </ul>
+  </div>
 </footer>
