79856b92d9ee416abe4ad890aa1b263126c2c061
ackindev <ackinlino123@gmail.com>
2024-12-16 09:44:58 -0300

    Estilização no footer e adição de slide com js puro

---
+++ site-transito-php/layout/footer.php
@@ -0,0 +1,27 @@
+<footer>
+  <div class="titulo_rodape">
+    <h1><img src="../assets/img/favicon.png" alt="Logo Leme" width="100px"><em>Coordenadoria Municipal de Trânsito</em></h1>
+    <h2><em>Secretaria de Segurança, Trânsito, Cidadania e Defesa Civil</em></h2>
+  </div>
+  <div>
+    <ul>
+      <li>
+      <i class="fas fa-location-dot"></i>  
+      Endereço
+      </li>
+      <li>
+      <i class="fas fa-phone"></i>
+      Telefone
+      </li>
+      <li>
+      <i class="fas fa-envelope"></i>  
+      Email</li>
+      <li>
+      <i class="fas fa-clock"></i>  
+      Atendimento</li>
+    </ul>
+  </div>
+</footer>
+<div class="direitos">
+  <p>&copy; Todos os direitos reservados</p>
+</div>
---
+++ site-transito-php/layout/slider.php
@@ -0,0 +1,25 @@
+<div class="slider-wrapper">
+  <button class="slide-arrow" id="slide-arrow-prev">
+    &#8249;
+  </button>
+  <button class="slide-arrow" id="slide-arrow-next">
+    &#8250;
+  </button>
+  <div class="slides-container" id="slides-container">
+    <div class="slide">
+      <a href="#" target="_blank">
+        <img src="../assets/img/img-slider7.png" alt="Imagem 1">
+      </a>
+    </div>
+    <div class="slide">
+      <a href="#" target="_blank">
+        <img src="../assets/img/img-slider8.png" alt="Imagem 2">
+      </a>
+    </div>
+    <div class="slide">
+      <a href="#" target="_blank">
+        <img src="../assets/img/img-slider9.jpg" alt="Imagem 3">
+      </a>
+    </div>
+  </div>
+</div>
