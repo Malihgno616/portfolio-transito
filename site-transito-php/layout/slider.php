26239a2b774058f1d17e734c1d2c087597e4ec30
ackindev <ackinlino123@gmail.com>
2025-01-02 11:05:34 -0300

    corrigindo slide

--- site-transito-php/layout/slider.php
+++ site-transito-php/layout/slider.php
@@ -1,25 +1,61 @@
-<div class="slider-wrapper animate__animated animate__backInLeft">
-  <button class="slide-arrow" id="slide-arrow-prev">
-    &#8249;
-  </button>
-  <button class="slide-arrow" id="slide-arrow-next">
-    &#8250;
-  </button>
-  <div class="slides-container" id="slides-container">
-    <div class="slide">
-      <a href="#" target="_blank">
-        <img src="../assets/img/img-slider7.png" alt="Imagem 1">
-      </a>
-    </div>
-    <div class="slide">
-      <a href="#" target="_blank">
-        <img src="../assets/img/img-slider8.png" alt="Imagem 2">
-      </a>
-    </div>
-    <div class="slide">
-      <a href="#" target="_blank">
-        <img src="../assets/img/img-slider9.jpg" alt="Imagem 3">
-      </a>
-    </div>
-  </div>
+<body>
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  <a href=""><img src="../assets/img/img-slider7.png" style="width:100%"></a>
 </div>
+
+<div class="mySlides fade">
+  <a href=""><img src="../assets/img/img-slider8.png" style="width:100%"></a>
+</div>
+
+<div class="mySlides fade">
+
+  <a href=""><img src="../assets/img/img-slider9.jpg" style="width:100%"></a>
+
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
+<script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+</script>
+
+</body>
+</html> 
