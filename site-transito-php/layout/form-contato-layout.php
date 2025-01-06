6858eccc63867ade561a7c46c0ee62b2b5ad9f81
ackindev <ackinlino123@gmail.com>
2025-01-06 11:40:56 -0300

    Refatorando o formulário de contato e os estilos

--- site-transito-php/layout/form-contato-layout.php
+++ site-transito-php/layout/form-contato-layout.php
@@ -1,16 +1,30 @@
-  <section class="container animate__animated animate__fadeIn">
+  <div class="titulo">
     <h1>Contato</h1>
-    <h3>Para entrar em contato, preencha este formulário</h3>
-    <form action="../config/database/form-contato-db.php" method="post" class="formulario-contato">
-      <div>
-        <input type="text" name="nome" autocomplete="off" required placeholder="Nome">
-        <input type="email" name="email" id="" required placeholder="Email">
-        <input type="text" name="telefone" id="" required placeholder="Telefone"> 
+    <h2>Entre em contato conosco preenchendo o formulário abaixo</h2>
+  </div>
+  <section class="container animate__animated animate__fadeIn"> 
+    <form action="../config/database/form-contato-db.php" method="post">
+      <div class="single-input">
+        <input type="text" name="nome" id="nome" required class="input">
+        <label for="nome">Nome...</label>
       </div>
-      <textarea name="mensagem" id="" required placeholder="Mensagem"></textarea>
-      <div class="btns">
-        <button>Limpar <i class="fa-solid fa-xmark"></i></button>
-        <button>Enviar <i class="fa-regular fa-paper-plane"></i></button>
+      <div class="single-input">
+        <input type="email" name="email" id="email" required class="input">
+        <label for="email">Email...</label>
+      </div>
+      <div class="single-input">
+        <input type="text" name="telefone" id="telefone" required class="input">
+        <label for="telefone">Telefone ou Celular...</label>
+      </div>
+      <div class="single-textarea">
+        <textarea name="mensagem" id="mensagem" required class="textarea" class="textarea"></textarea>        
+        <label for="mensagem">Digite sua mensagem...</label>
+      </div>
+      <div class="buttons">
+        <button>Limpar <i class="fa-solid fa-broom"></i></button>
+        <button type="submit">
+          Enviar <i class="fas fa-paper-plane"></i>
+        </button>
       </div>
     </form>
   </section>
