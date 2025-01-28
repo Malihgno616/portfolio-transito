1e6ed8fae10f8fa8332b1dc0d2a795304c76bb09
ackindev <ackinlino123@gmail.com>
2025-01-28 09:57:27 -0300

    Refazendo o footer do site

--- layout/footer.php
+++ layout/footer.php
@@ -1,45 +1,38 @@
 <footer>
-  <div class="titulo_rodape">
-    <img src="../assets/img/favicon.png" alt="Logo Leme" width="100px">
-    <h1><em>Coordenadoria Municipal de Trânsito</em></h1>
-    <h2><em>Secretaria de Segurança, Trânsito, Cidadania e Defesa Civil</em></h2>
-  </div>
-  <div>
-    <ul>
-      <li>
-      <i class="fas fa-location-dot"></i>  
-      R. Dr.Armando Sales de Oliveira, 925 - Centro, Leme - SP, 13610-220
-      </li>
-      <li>
-      <i class="fas fa-phone"></i>
+  <div class="footer-container">
+      <div class="titulo-footer">
+        <img src="../assets/img/favicon.png" alt="logo Transito Leme" width="80px">
+        <h2>Coordenadoria Municipal de Trânsito</h2>
+      </div>
+      <em>Secretaria de Segurança, Trânsito, Cidadania e Defesa Civil</em>
+      <p><a href="#"><i class="fa-solid fa-location-dot"></i>
+      R. Dr. Armando Sales de Oliveira, 925 - Centro, Leme - SP, 13610-220
+      </a></p>
+      <p><a href="#"><i class="fa-solid fa-clock"></i>
+      Horario de atendimento: Segunda a Sexta feira das 8:00Hrs as 16:00Hrs
+      </a></p>
+      <p><a href="#"><i class="fas fa-phone"></i>
       (19) 3573-5310
-      </li>
-      <li>
-      <i class="fas fa-envelope"></i>  
-      nucleodetransito@leme.sp.gov.br</li>
-      <li>
-      <i class="fas fa-clock"></i>  
-      Horário de atendimento: Segunda à Sexta-feira das 8:00 às 16:00</li>
-    </ul>
-  </div>    
+      </a></p>
+      <p><a href="#"><i class="fa-solid fa-envelope"></i>
+       nucleodetransito@leme.sp.gov.br 
+      </a></p>
+  </div>
+  <div class="footer-container">
+    <h2>Links Úteis</h2>
+    <ul class="links-uteis">
+      <li><a href="#">Prefeitura de Leme</a></li>
+      <li><a href="#">Câmara Municipal de Leme</a></li>
+      <li><a href="#">Detran-SP</a></li>
+      <li><a href="#">Denatran</a></li>
+      <li><a href="#">IPVA-SP</a></li>
+      <li><a href="#">E-CNH</a></li>
+      <li><a href="#">DER-SP</a></li>
+    </ul>    
+  </div>
+  
 </footer>
-<h1 style="background-color: #736A4F; text-align:center; font-size:2rem; color: #f2f2f2;">Links Úteis</h1>  
-  <ul class="links-uteis">
-  <li><a href="https://www.lemesp.gov.br/" target="_blank">Prefeitura de Leme</a>
-    <li><a href="https://camaraleme.sp.gov.br/">Câmara Municipal de Leme</a></li>
-    <li><a href="https://www.detran.sp.gov.br/wps/portal/portaldetran/cidadao/home">Detran-SP</a></li>
-    <li><a href="http://www.denatran.gov.br/">Denatran</a></li>
-    <li><a href="https://www.ipva.fazenda.sp.gov.br/ipvanet/">IPVA-SP</a></li>
-    <li><a href="https://www.e-cnhsp.sp.gov.br/">E-CNH</a></li>
-    <li><a href="http://www.der.sp.gov.br/WebSite/Index.aspx">DER-SP</a></li>
-  </ul>
-  <div class="contatos">
-    <h2>Meios de Contato <i class="fas fa-phone"></i> </h2>
-    <a id="zapzap" href="#"><i class="fa-brands fa-whatsapp"></i> WhatsApp</a>
-    <a href="#"><i class="fas fa-envelope"></i>  nucleodetransito@leme.sp.gov.br
-    </a>
-    <a href="#"><i class="fas fa-phone"></i> (19) 3573-5310 </a>
-  </div>
-  <div class="direitos">
-    <p>&copy; Todos os direitos reservados</p>
-  </div>
+<div class="direitos">
+  <p>&copy; Todos os direitos reservados, desenvolvido por <a href="#" target="_blank">Ackin.Dev</a>.
+  </p>
+</div>
