ae1d71616a9154e6d095116a892fc01d3a2231b8
ackindev <ackinlino123@gmail.com>
2025-02-07 10:35:59 -0300

    Formulário do Idoso refeito

--- layout/form-idoso-layout.php
+++ layout/form-idoso-layout.php
@@ -37,7 +37,6 @@ $array_generos = [
 
 ?>
 
-
 <div class="container">
 
 	<div class="title">
@@ -50,100 +49,99 @@ $array_generos = [
 		
 		<div class="input-group">
 			<input type="text" id="nome" name="nome-idoso" required class="input"/>
-			<label for="nome" id="label-input">Nome do Idoso</label>
+			<label for="nome" class="label-input">Nome do Idoso</label>
 		</div>
 		
 		<div class="input-group">
+			<label for="nasc-idoso" class="label-date">Data de Nascimento</label>
 			<input type="date" name="nascimento-idoso" required class="input-date">
-			<label for="nasc-idoso" id="label-input">Data de Nascimento</label>
 		</div>
 		
 		<div class="input-group">
+			<label for="genero" class="label-select">Sexo</label>
 			<select name="genero-idoso" id="genero-idoso" required class="select">
 				<option value="selecione">Selecione...</option>
 				<?php foreach($array_generos as $generos){
 					echo "<option value='$generos'>$generos</option>";
 				};?>
 			</select>
-			<label for="genero" id="label-input">Sexo</label>
 		</div>
-		
 		
 		<div class="input-group">
 			<input type="text" name="endereco-idoso" required class="input">
-			<label for="endereco" id="label-input">Endereço(RUA, AV)</label>
+			<label for="endereco" class="label-input">Endereço(RUA, AV)</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="numero-endereco-idoso" required class="input">
-			<label for="numero" id="label-input">Número</label>
+			<label for="numero" class="label-input">Número</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="complemento-idoso" class="input">
-			<label for="complemento" id="label-input">Complemento(opcional)</label>
+			<label for="complemento" class="label-input">Complemento(opcional)</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="bairro-idoso" required class="input">
-			<label for="bairro" id="label-input">Bairro</label>
+			<label for="bairro" class="label-input">Bairro</label>
 		</div>
 
 		<div class="input-group">
 			<input type="text" name="cep-idoso" required class="input">
-			<label for="cep" id="label-input">CEP</label>
+			<label for="cep" class="label-input">CEP</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="cidade-idoso" required class="input">
-			<label for="cidade" id="label-input">Cidade</label>
+			<label for="cidade" class="label-input">Cidade</label>
 		</div>
 		
 		<div class="input-group">
+			<label for="uf" class="label-select">
+				UF(Unidade Federal)
+			</label>
 			<select name="uf-idoso" class="select" required>
 				<option value="selecione">Selecione...</option>
 				<?php foreach($estados as $uf){
 					echo "<option value='$uf'>$uf</option>";
 				}?>
 			</select>
-			<label for="uf" id="label-input">
-				UF(Unidade Federal)
-			</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="telefone-idoso" required class="input">
-			<label for="telefone" id="label-input">Telefone</label>
+			<label for="telefone" class="label-input">Telefone</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="rg-idoso" required class="input">
-			<label for="rg" id="label-input">RG</label>
+			<label for="rg" class="label-input">RG</label>
 		</div>
 
 		<div class="input-group">
+			<label for="data-expedicao" class="label-date">Data de Expedição</label>
 			<input type="date" name="data-expedicao-idoso" required class="input-date">
-			<label for="data-expedicao" id="label-input">Data de Expedição</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="expedido-idoso" required class="input">
-			<label for="expedido-por" id="label-input">Expedido por</label>
+			<label for="expedido-por" class="label-input">Expedido por</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="cnh-idoso" class="input">
-			<label for="cnh" id="label-input">CNH(Se for condutor)</label>
+			<label for="cnh" class="label-input">CNH(Se for condutor)</label>
 		</div>
 		
 		<div class="input-group">
+			<label for="validade-cnh" class="label-date">Validade da CNH</label>
 			<input type="date" name="validade-cnh-idoso" class="input-date">
-			<label for="validade-cnh" id="label-input">Validade da CNH</label>
 		</div>
 		
 		<div class="input-group">
 			<input type="text" name="email-idoso" class="input">
-			<label for="email" id="label-input">Digite seu email(opcional)</label>
+			<label for="email" class="label-input">Digite seu email(opcional)</label>
 		</div>
 		
 		<div class="input-group">
@@ -171,12 +169,11 @@ $array_generos = [
 			<div class="input-file-container">
 				
 				<div class="input-file">
-					<input type="file" name="copia-rg-idoso" id="file-input" required placeholder="Cópia do RG" class="file-input file" accept="image/*">
+					<input type="file" name="copia-rg-idoso" id="file-input" required placeholder="Cópia do RG" class="file" accept="image/*">
 					<label for="copia-rg-idoso">Selecione(JPG, PNG ou PDF) a cópia do RG do idoso ou documento equivalente <strong>(OBRIGATÓRIO)</strong></label>
 					<span id="file-name"></span>						
 				</div>
-				<div id="image-preview"></div>
-				
+								
 			</div>
 			
 		</div>
@@ -187,74 +184,74 @@ $array_generos = [
 				
 					<div class="input-group" id="representante">
 						<input type="text" name="nome-representante" class="input" />
-						<label for="nome-representante" id="label-input">Nome do representante</label>
+						<label for="nome-representante" class="label-input">Nome do representante</label>
 					</div>
 					
 					<div class="input-group" id="representante">
 						<input type="text" name="email-representante" class="input" />
-						<label for="email-representante" id="label-input">Email</label>
+						<label for="email-representante" class="label-input">Email</label>
 					</div>
 					
 					<div class="input-group" id="representante">
 						<input type="text" name="endereco-representante" class="input" />
-						<label for="endereco-representante" id="label-input">Endereço(RUA, AV)</label>
+						<label for="endereco-representante" class="label-input">Endereço(RUA, AV)</label>
 					</div>
 					
 					<div class="input-group" id="representante">
 						<input type="text" name="numero-endereco-representante" class="input" />
-						<label for="numero-representante" id="label-input">Nº</label>
+						<label for="numero-representante" class="label-input">Nº</label>
 					</div>
 					
 					<div class="input-group" id="representante">
 						<input type="text" name="complemento-representante" class="input" />
-						<label for="complemento-representante" id="label-input">Complemento</label>
+						<label for="complemento-representante" class="label-input">Complemento</label>
 					</div>
 					
 					<div class="input-group" id="representante">
 						<input type="text" name="bairro-representante" class="input" />
-						<label for="bairro-representante" id="label-input">Bairro</label>
+						<label for="bairro-representante" class="label-input">Bairro</label>
 					</div>
 					
 					<div class="input-group" id="representante">
 						<input type="text" name="cep-representante" class="input">
-						<label for="cep-representante" id="label-input">CEP</label>
+						<label for="cep-representante" class="label-input">CEP</label>
 					</div>
 					
 					<div class="input-group" id="representante">
 						<input type="text" name="cidade-representante" class="input" />
-						<label for="cidade-representante" id="label-input">Cidade</label>
+						<label for="cidade-representante" class="label-input">Cidade</label>
 					</div>
 					
 					<div class="input-group">
+						<label for="uf-representante" class="label-select">
+							UF(Unidade Federal)
+						</label>
 						<select name="uf-representante" class="select">
 							<option value="" >Selecione...</option>
 							<?php foreach($estados as $uf){
 								echo "<option value='$uf'>$uf</option>";
 							}?>
 						</select>
-						<label for="uf-representante" id="label-input">
-							UF(Unidade Federal)
-						</label>
 					</div>
 
 					<div class="input-group" id="representante">
 						<input type="text" name="telefone-representante" class="input" />
-						<label for="telefone-representante" id="label-input">Telefone</label>
+						<label for="telefone-representante" class="label-input">Telefone</label>
 					</div>
 					
 					<div class="input-group" id="representante">
 						<input type="text" name="rg-representante" class="input">
-						<label for="rg-representante" id="label-input">RG</label>
+						<label for="rg-representante" class="label-input">RG</label>
 					</div>
 					
 					<div class="input-group" id="representante">
+						<label for="" class="label-date">Data de Expedição</label>
 						<input type="date" name="expedicao-representante" id="" class="input-date">
-						<label for="" id="label-input">Data de Expedição</label>
 					</div>
 					
 					<div class="input-group">
 						<input type="text" name="expedido-representante" class="input">
-						<label for="expedido-por" id="label-input">Expedido por</label>
+						<label for="expedido-por" class="label-input">Expedido por</label>
 					</div>
 					
 					<div class="input-group" id="representante">
@@ -265,8 +262,7 @@ $array_generos = [
 								<label for="copia-rg-representante">Selecione(JPG, PNG ou PDF)  a cópia do RG do representante ou documento equivalente <strong>(OBRIGATÓRIO)</strong></label>
 								<span id="file-name"></span>						
 							</div>
-							<div id="image-preview"></div>
-							
+														
 						</div>
 					</div>
 					
@@ -278,7 +274,6 @@ $array_generos = [
 								<label for="">Selecione(JPG, PNG ou PDF) o comprovante de representante legal <strong>(OBRIGATÓRIO)</strong></label>
 								<span id="file-name"></span>						
 							</div>
-							<div id="image-preview"></div>
 							
 						</div>
 					</div>
@@ -292,8 +287,7 @@ $array_generos = [
 					</button>
 				</div>
 				
-			</div>	
-			
+			</div>			
 
 		</form>
 		
