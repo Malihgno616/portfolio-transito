d5844ee16e44c1711bdebc5db70417a7c30a07ae
Ackin.Dev <ackinlino123@gmail.com>
2025-02-18 11:59:11 -0300

    Deletando o arquivo htacess e alterando o destindo dos links dos links úteis

--- layout/footer.php
+++ layout/footer.php
@@ -5,7 +5,7 @@
         <h2>Coordenadoria Municipal de Trânsito</h2>
       </div>
       <em>Secretaria de Segurança, Trânsito, Cidadania e Defesa Civil</em>
-      <p><a href="#"><i class="fa-solid fa-location-dot"></i>
+      <p><a href="https://www.google.com.br/maps/place/Secretaria+De+Seguran%C3%A7a,+Tr%C3%A2nsito,+Cidadania+e+Defesa+Civil/@-22.1878286,-47.3904941,19z/data=!4m15!1m8!3m7!1s0x94c80b7713b22f0f:0x41b5d305ee09d98c!2sLeme,+SP!3b1!8m2!3d-22.1889212!4d-47.3967766!16s%2Fg%2F11bc5_kyms!3m5!1s0x94c80ba208d12a13:0xb2e8e6492e6fa10b!8m2!3d-22.1878319!4d-47.3903743!16s%2Fg%2F1w347ql5?hl=pt-BR&entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoASAFQAw%3D%3D"><i class="fa-solid fa-location-dot"></i>
       R. Dr. Armando Sales de Oliveira, 925 - Centro, Leme - SP, 13610-220
       </a></p>
       <p><a href="#"><i class="fa-solid fa-clock"></i>
@@ -21,13 +21,13 @@
   <div class="footer-container">
     <h2>Links Úteis</h2>
     <ul class="links-uteis">
-      <li><a href="#">Prefeitura de Leme</a></li>
-      <li><a href="#">Câmara Municipal de Leme</a></li>
-      <li><a href="#">Detran-SP</a></li>
-      <li><a href="#">Denatran</a></li>
-      <li><a href="#">IPVA-SP</a></li>
-      <li><a href="#">E-CNH</a></li>
-      <li><a href="#">DER-SP</a></li>
+      <li><a href="https://www.leme.sp.gov.br/">Prefeitura de Leme</a></li>
+      <li><a href="https://www.leme.sp.leg.br/">Câmara Municipal de Leme</a></li>
+      <li><a href="https://www.detran.sp.gov.br/wps/portal/portaldetran/cidadao/home">Detran-SP</a></li>
+      <li><a href="http://www.denatran.gov.br/">Denatran</a></li>
+      <li><a href="https://www.ipva.fazenda.sp.gov.br/ipvanet/">IPVA-SP</a></li>
+      <li><a href="https://www.e-cnhsp.sp.gov.br/">E-CNH</a></li>
+      <li><a href="http://www.der.sp.gov.br/WebSite/Index.aspx">DER-SP</a></li>
     </ul>    
   </div>
   
