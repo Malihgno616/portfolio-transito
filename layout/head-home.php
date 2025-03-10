942360168ac37a5f80818e4b1a61e4209514158c
Ackin.Dev <ackinlino123@gmail.com>
2025-03-10 11:50:00 -0300

    Excluindo uma tag link do estilo que não existe

--- layout/head-home.php
+++ layout/head-home.php
@@ -4,8 +4,9 @@
 <link rel="stylesheet" href="../assets/css/slide.css">
 <link rel="stylesheet" href="../assets/css/cards.css">
 <link rel="stylesheet" href="../assets/css/institucional.css">
-<link rel="stylesheet" href="../assets/css/section.css">
 <link rel="stylesheet" href="../assets/css/servicos-destaque.css">
+<link rel="stylesheet" href="../assets/css/root.css">
+<link rel="stylesheet" href="../assets/css/dados-beneficiario.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="shortcut icon" href="../assets/img/favicon.png" type="image/x-icon">
 <link
