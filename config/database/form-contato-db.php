4a1e2bc745191f6c5a8046e17e157be4d582442f
Ackin.Dev <ackinlino123@gmail.com>
2025-04-11 11:18:58 -0300

    Adicionando um alert caso for enviado as informações do formulário

--- config/database/form-contato-db.php
+++ config/database/form-contato-db.php
@@ -52,76 +52,14 @@ mysqli_stmt_bind_param($stmt, "ssss", $nome, $email, $telefone, $mensagem);
 
 $executed = mysqli_stmt_execute($stmt);
 
+if ($executed) {
+  $_SESSION['sucesso'] = "Mensagem enviada com sucesso!";
+  header("Location: ../../pages/contato.php");
+  exit;
+} else {
+  $_SESSION['erro-sql'] = "Erro ao enviar os dados: " . mysqli_stmt_error($stmt);
+  header("Location: ../../pages/contato.php");
+  exit;
+}
+
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Enviado</title>
-  <style>
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      font-family: Arial, Helvetica, sans-serif;
-    }
-    div {
-      width: 500px;
-      height: 200px;
-      background-color: #f0f0f0;
-      margin: 225px auto;
-      padding: 20px;
-      border: 1px solid #ccc;
-      border-radius: 10px;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-    }
-    p {
-      font-size: 2.1rem;
-      text-align: center;
-      margin-bottom: 1rem;
-    }
-    a {
-      text-decoration: none;
-      color: #f0f0f0;
-      background-color: rgb(255, 73, 73);
-      padding: .5rem;
-      font-size: 1.25rem;
-      border-radius: 10rem;
-      transition: all .5s ease;
-    }
-    a:hover {
-      filter: brightness(1.1);
-    }
-    @media(max-width: 480px){
-      div {
-        width: 100%;
-      }
-      p {
-        font-size: 1.5rem;
-      }
-
-    }
-  </style>
-</head>
-<body>
-  <div>
-    <?php
-    // Verifica se a execução da consulta foi bem-sucedida
-    if ($executed) {
-      echo "<p>Dados enviados com sucesso!</p>";
-    } else {
-      echo "<p>Erro ao enviar dados: " . mysqli_error($conn) . "</p>";
-    }
-
-    // Fecha a conexão com o banco de dados
-    mysqli_close($conn);
-    ?>
-    <a href="../../public/index.php">Clique para voltar à página principal</a>
-  </div>
-</body>
-</html>
--- layout/form-contato-layout.php
+++ layout/form-contato-layout.php
@@ -1,16 +1,23 @@
 <?php 
 session_start(); 
 $erro = isset($_SESSION['erro']) ? $_SESSION['erro'] : null;
+$sucesso_sql = isset($_SESSION['sucesso']) ? $_SESSION['sucesso'] : null;
+$erro_sql = isset($_SESSION['erro-sql']) ? $_SESSION['erro-sql'] : null ;
+
+unset($_SESSION['sucesso']);
 unset($_SESSION['erro']);
+unset($_SESSION['erro-sql']);
+
 ?>
 <form class="max-w-200 mx-auto m-20 p-5 border-2 border-gray-200 rounded-md animate__animated animate__fadeIn" action="../config/database/form-contato-db.php" method="post">
+
   <?php if($erro): ?>
     <div id="alert-border-2" class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50  dark:border-red-800" role="alert">
         <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
         </svg>
         <div class="ms-3 text-sm font-medium">
-          <?=$erro; ?>
+          <p class="text-lg md:text-md"><?=$erro; ?></p>
         </div>
         <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8"  data-dismiss-target="#alert-border-2" aria-label="Close">
           <span class="sr-only">Dismiss</span>
@@ -20,6 +27,23 @@ unset($_SESSION['erro']);
         </button>
       </div>
   <?php endif; ?>
+
+  <?php if($sucesso_sql): ?>
+      <div id="alert-border-3" class="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50" role="alert">
+        <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        <div class="ms-3 text-sm font-medium">
+          <p class="text-lg md:text-md"><?=$sucesso_sql;?></p>
+        </div>
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8"  data-dismiss-target="#alert-border-3" aria-label="Close">
+          <span class="sr-only">Dismiss</span>
+          <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+          </svg>
+        </button>
+      </div>     
+    <?php endif;?>
 
   <div class="container mx-auto p-10 text-center">
     <h1 class="text-5xl p-3 md:text-3xl">Contato</h1>
