b64d1c78982971647f1d22860f3c2f0fe0097b11
Ackin.Dev <ackinlino123@gmail.com>
2025-04-22 10:10:05 -0300

    Inserindo flowbite na página do cartão deficiente

--- layout/cartao-deficiente.php
+++ layout/cartao-deficiente.php
@@ -38,37 +38,40 @@
   }    
 ?>
 
-<div class="texto-format">
-  <h1>Cartão deficiente</h1>
-  <p>Siga as etapas para solicitar o cartão</p>
-  <p>Assim que o cartão estiver pronto, será feito para agendamento da retirada do cartão</p>
+<div class="flex flex-col justify-center items-center m-20 gap-3">
+  <h1 class="text-5xl md:text-2xl font-bold text-center">Cartão deficiente</h1>
+  <p class="text-lg md:text-md text-justify">Siga as etapas para solicitar o cartão</p>
+  <p class="text-lg md:text-md text-justify">Assim que o cartão estiver pronto, será feito para agendamento da retirada do cartão</p>
 </div>
 
-<form action="<?= pagination(); ?>" method="get" class="solicitacoes animate__animated animate__fadeIn">
-  <h2>Selecione o tipo de solicitação</h2>
-  <div class="radio-container">
-    <input type="radio" name="solicitacao" id="cartao1" value="1">
-    <label for="cartao1">Desejo fazer meu 1º cartão</label>
+<form action="<?= pagination(); ?>" method="get" class="space-y-6 animate__animated animate__fadeIn mt-8 mb-8 w-100 m-auto p-10 rounded-lg shadow-md">
+  <h2 class="text-xl font-semibold">Selecione o tipo de solicitação</h2>
+
+  <div class="space-y-4">
+    <div class="flex items-center">
+      <input id="cartao1" type="radio" name="solicitacao" value="1" class="w-4 h-4 text-yellow-500 bg-gray-100 border-gray-300 focus:ring-yellow-500">
+      <label for="cartao1" class="ml-2 text-md font-medium text-gray-900">Desejo fazer meu 1º cartão</label>
+    </div>
+
+    <div class="flex items-center">
+      <input id="cartao2" type="radio" name="solicitacao" value="2" class="w-4 h-4 text-yellow-500 bg-gray-100 border-gray-300 focus:ring-yellow-500">
+      <label for="cartao2" class="ml-2 text-md font-medium text-gray-900">Desejo renovar meu cartão</label>
+    </div>
+
+    <div class="flex items-center">
+      <input id="cartao3" type="radio" name="solicitacao" value="3" class="w-4 h-4 text-yellow-500 bg-gray-100 border-gray-300 focus:ring-yellow-500">
+      <label for="cartao3" class="ml-2 text-md font-medium text-gray-900">Desejo cancelar meu cartão</label>
+    </div>
+
+    <div class="flex items-center">
+      <input id="cartao4" type="radio" name="solicitacao" value="4" class="w-4 h-4 text-yellow-500 bg-gray-100 border-gray-300 focus:ring-yellow-500">
+      <label for="cartao4" class="ml-2 text-md font-medium text-gray-900">Preciso de 2ª via do cartão (Dentro do prazo de validade)</label>
+    </div>
   </div>
 
-  <div class="radio-container">
-    <input type="radio" name="solicitacao" id="cartao2" value="2">
-    <label for="cartao2">Desejo renovar meu cartão</label>
-  </div>
-
-  <div class="radio-container">
-    <input type="radio" name="solicitacao" id="cartao3" value="3">
-    <label for="cartao3">Desejo cancelar meu cartão</label>
-  </div>
-
-  <div class="radio-container">
-    <input type="radio" name="solicitacao" id="cartao4" value="4">
-    <label for="cartao4">Preciso de 2ª via do cartão (Dentro do prazo de validade)</label> 
-  </div>
-
-  <div class="motivo">
-    <label for="motivos">Motivo</label>
-    <select name="motivos">
+  <div class="mt-6">
+    <label for="motivos" class="block mb-2 text-md font-medium text-gray-900">Motivo</label>
+    <select id="motivos" name="motivos" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5">
       <option value="">Selecione...</option>
       <option value="1">Perda</option>
       <option value="2">Furto</option>
@@ -77,7 +80,9 @@
     </select>
   </div>
 
-  <button type="submit">Próximo</button>
+  <button type="submit" class="text-white bg-yellow-500 hover:bg-yellow-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center duration-200">
+    Próximo
+  </button>
 </form>
 
 <script src="../assets/js/exibirSelect.js"></script>
--- pages/cartao-deficiente.php
+++ pages/cartao-deficiente.php
@@ -5,9 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Cartão Deficiente</title>
   <link rel="shortcut icon" href="../assets/img/favicon.png" type="image/x-icon">
-  <?php 
-    include('../layout/head-card-deficiente.php');
-  ?>
+  <!-- <?php  include('../layout/head-card-deficiente.php') ;?> -->
+  <?php include('../layout/head.php'); ?>
+
 </head>
 <body>
   <?php 
