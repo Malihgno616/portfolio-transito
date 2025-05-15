7e1627ea609e556fb13eba88c44645dc34b63f30
Ackin.Dev <ackinlino123@gmail.com>
2025-05-15 13:53:35 -0300

    Mudando a cor dos checkboxes no form deficiente

--- layout/layout-form-deficiente.php
+++ layout/layout-form-deficiente.php
@@ -535,7 +535,7 @@ $deficiencias = [
     <?php foreach($deficiencias as $deficiencia): ?>
     <div class="p-5">
       <input type="checkbox"
-        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2"
+        class="w-4 h-4 text-yellow-500 bg-gray-100 border-gray-300 rounded-sm focus:ring-yellow-500 focus:ring-2"
         name="deficiencia-ambulatoria[]" value="<?=$deficiencia?>">
       <label for="deficiencia" class="ms-2 text-lg font-medium text-gray-900">
         <?= $deficiencia; ?>
