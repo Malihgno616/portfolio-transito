0dc0642e78f8a643611d57218b25b74cb5ac62ee
Ackin.Dev <ackinlino123@gmail.com>
2025-05-16 08:49:09 -0300

    Exibição das imagens do representante na renovação do cartão deficiente

--- layout/dados-bene-renova.php
+++ layout/dados-bene-renova.php
@@ -540,30 +540,54 @@ $dados_beneficiario = $_SESSION['dados_beneficiario_renova'];
 
     <div class="relative mb-5">
 
-      <label for=""
+      <h2 class="text-3xl p-2 text-center">RG do representante</h2>
+
+      <?php if (!empty($dados_beneficiario['copia_rg_representante'])):?>
+        <label for=""
         class="flex flex-col items-center justify-center w-full h-74 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-yellow-700 hover:bg-gray-100">
         <div class="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg class="w-8 h-8 mb-4 text-yellow-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-              viewBox="0 0 20 16">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-            </svg>
-            <p class="mb-2 text-xl text-center text-gray-500 dark:text-gray-400">
-              <span class="font-semibold">Selecione uma cópia digitalizada do RG do representante </span>
-            </p>
-            <p class="text-lg text-gray-500 text-center dark:text-gray-400">
-              JPG, PNG ou PDF <strong>(OBRIGATÓRIO)</strong>
-            </p>
-
+            <img class="w-full h-56 object-cover md:w-30" src="data:image/*;base64, <?=base64_encode($dados_beneficiario['copia_rg_representante']);?>" alt="">
         </div>
         <input type="file" name="" id="" class="hidden">
         <span id="file-name"></span>
       </label>
-
+        <?php else: ?>
+          <label for=""
+            class="flex flex-col items-center justify-center w-full h-74 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-yellow-700 hover:bg-gray-100">
+            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+              <svg class="w-8 h-8 mb-4 text-yellow-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                  viewBox="0 0 20 16">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                </svg>
+                <p class="mb-2 text-xl text-center text-gray-500 dark:text-gray-400">
+                  <span class="font-semibold">Selecione uma cópia digitalizada do RG do representante </span>
+                </p>
+                <p class="text-lg text-gray-500 text-center dark:text-gray-400">
+                  JPG, PNG ou PDF <strong>(OBRIGATÓRIO)</strong>
+                </p>
+            </div>
+            <input type="file" name="" id="" class="hidden">
+            <span id="file-name"></span>
+          </label>
+      <?php endif;?>
     </div>
 
-    <div class="relative mb-5">
-        
+    <div class="relative mb-5">      
+
+      <h2 class="text-3xl p-2 text-center">Comprovante do representante</h2>
+
+      <?php if (!empty($dados_beneficiario['comprovante_representante'])):?>
+        <label for=""
+        class="flex flex-col items-center justify-center w-full h-74 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-yellow-700 hover:bg-gray-100">
+          <div class="flex flex-col items-center justify-center pt-5 pb-6">
+              <img class="w-full h-56 object-cover md:w-30" src="data:image/*;base64, <?=base64_encode($dados_beneficiario['comprovante_representante']);?>">
+          </div>
+          <input type="file" name="" id="" class="hidden">
+          <span id="file-name"></span>
+        </label>
+      
+      <?php else: ?>
 
       <label for=""
         class="flex flex-col items-center justify-center w-full h-74 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:bg-yellow-700 hover:bg-gray-100">
@@ -583,6 +607,8 @@ $dados_beneficiario = $_SESSION['dados_beneficiario_renova'];
         <input type="file" name="" id="" class="hidden">
         <span id="file-name"></span>
       </label>
+
+      <?php endif;?>
 
     </div>
   </div>
