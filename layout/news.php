b8d9b4d65bd470ead0d5c08477ea11cab1d0af40
Ackin.Dev <ackinlino123@gmail.com>
2025-05-29 10:02:51 -0300

    mudando a cor dos botões dos cards e servicos em destaque

--- layout/news.php
+++ layout/news.php
@@ -20,7 +20,7 @@
           <p class="mb-3 text-sm lg:text-base text-gray-900 line-clamp-3">Here are the biggest enterprise technology
             acquisitions of 2021 so far, in reverse chronological order.</p>
           <a href="#"
-            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-yellow-400 duration-75">
             Read more
             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
               fill="none" viewBox="0 0 14 10">
--- layout/servicos-destaque.php
+++ layout/servicos-destaque.php
@@ -12,7 +12,7 @@
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Clique aqui para preencher o formulário do cartão
           do idoso.</p>
         <a href="../pages/formulario-idoso.php"
-          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-yellow-300 duration-75">
           Clique aqui
           <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
             viewBox="0 0 14 10">
@@ -29,7 +29,7 @@
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Clique aqui para preencher o formulário do cartão
           do deficiente.</p>
         <a href="../pages/form-deficiente.php"
-          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black bg-yellow-500 rounded-lg hover:bg-yellow-200 focus:ring-4 focus:outline-none focus:ring-yellow-300 duration-75">
           Clique aqui
           <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
             viewBox="0 0 14 10">
