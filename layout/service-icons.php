f6695e0a8745d4c983a16bec1d90fdc04692631a
Ackin.Dev <ackinlino123@gmail.com>
2025-06-03 11:00:15 -0300

    Ajuste nos ícones de serviços online

--- layout/service-icons.php
+++ layout/service-icons.php
@@ -10,8 +10,8 @@
     <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
       <!-- Item de Serviço -->
       <li
-        class="flex flex-col items-center border-2 border-black rounded-full p-4 hover:border-yellow-500 transition duration-300">
-        <a class="text-base sm:text-xl text-black p-4 flex flex-col items-center justify-center hover:text-gray-500 duration-200"
+        class="flex flex-col items-center p-4 hover:border-yellow-500 transition duration-300">
+        <a class="text-base sm:text-xl te-black p-4 flex flex-col items-center justify-center hover:text-gray-700 duration-200"
           href="../pages/multas.php">
           <div class="icones mb-2">
             <i class="fas fa-car text-3xl sm:text-4xl md:text-5xl"></i>
@@ -22,8 +22,8 @@
 
       <!-- Outros itens seguem o mesmo padrão -->
       <li
-        class="flex flex-col items-center border-2 border-black rounded-full p-4 hover:border-yellow-500 transition duration-300">
-        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-500 duration-200"
+        class="flex flex-col items-center p-4 hover:border-yellow-500 transition duration-300">
+        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-700 duration-200"
           href="../pages/vagas-especiais.php">
           <div class="mb-2">
             <i class="fas fa-wheelchair text-3xl sm:text-4xl md:text-5xl"></i>
@@ -32,8 +32,8 @@
         </a>
       </li>
       <li
-        class="flex flex-col items-center border-2 border-black rounded-full p-4 hover:border-yellow-500 transition duration-300">
-        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-500 duration-200"
+        class="flex flex-col items-center p-4 hover:border-yellow-500 transition duration-300">
+        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-700 duration-200"
           href="../pages/sinalizacao.php">
           <div class="mb-2">
             <i class="fas fa-ban text-3xl sm:text-4xl md:text-5xl"></i>
@@ -42,8 +42,8 @@
         </a>
       </li>
       <li
-        class="flex flex-col items-center border-2 border-black rounded-full p-4 hover:border-yellow-500 transition duration-300">
-        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-500 duration-200"
+        class="flex flex-col items-center p-4 hover:border-yellow-500 transition duration-300">
+        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-700 duration-200"
           href="../pages/interdicoes.php">
           <div class="mb-2">
             <i class="fas fa-sign text-3xl sm:text-4xl md:text-5xl"></i>
@@ -52,8 +52,8 @@
         </a>
       </li>
       <li
-        class="flex flex-col items-center border-2 border-black rounded-full p-4 hover:border-yellow-500 transition duration-300">
-        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-500 duration-200"
+        class="flex flex-col items-center p-4 hover:border-yellow-500 transition duration-300">
+        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-700 duration-200"
           href="../pages/semaforos.php">
           <div class="mb-2">
             <i class="fas fa-traffic-light text-3xl sm:text-4xl md:text-5xl"></i>
@@ -62,8 +62,8 @@
         </a>
       </li>
       <li
-        class="flex flex-col items-center border-2 border-black rounded-full p-4 hover:border-yellow-500 transition duration-300">
-        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-500 duration-200"
+        class="flex flex-col items-center p-4 hover:border-yellow-500 transition duration-300">
+        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-700 duration-200"
           href="../pages/lombadas.php">
           <div class="mb-2">
             <i class="fa-solid fa-mound text-3xl sm:text-4xl md:text-5xl"></i>
@@ -72,8 +72,8 @@
         </a>
       </li>
       <li
-        class="flex flex-col items-center border-2 border-black rounded-full p-4 hover:border-yellow-500 transition duration-300">
-        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-500 duration-200"
+        class="flex flex-col items-center p-4 hover:border-yellow-500 transition duration-300">
+        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-700 duration-200"
           href="../pages/zona-azul.php">
           <div class="mb-2">
             <i class="fas fa-location-dot text-3xl sm:text-4xl md:text-5xl"></i>
@@ -82,8 +82,8 @@
         </a>
       </li>
       <li
-        class="flex flex-col items-center border-2 border-black rounded-full p-4 hover:border-yellow-500 transition duration-300">
-        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-500 duration-200"
+        class="flex flex-col items-center p-4 hover:border-yellow-500 transition duration-300">
+        <a class="text-base sm:text-xl p-4 flex flex-col items-center justify-center hover:text-gray-700 duration-200"
           href="../pages/bicicletas.php">
           <div class="mb-2">
             <i class="fas fa-bicycle text-3xl sm:text-4xl md:text-5xl"></i>
