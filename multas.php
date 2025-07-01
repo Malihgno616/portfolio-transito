0164365f057da2cedebe57a79e5a6a130122299b
Ackin.Dev <ackinlino123@gmail.com>
2025-07-01 13:35:41 -0300

    Removendo um link html

--- multas.php
+++ multas.php
@@ -4,7 +4,6 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- <?php include('../layout/head-format.php');?> -->
   <?php include('layout/head.php');?>
   <title>Multas</title>
 </head>
