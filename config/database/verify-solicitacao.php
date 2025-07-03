16006be4af148a763be44ef6c8895fe9ceecddce
Ackin.Dev <ackinlino123@gmail.com>
2025-07-03 11:28:38 -0300

    Verificação das solicitações de perdas e danos dos cartões

---
+++ config/database/verify-solicitacao.php
@@ -0,0 +1,81 @@
+<?php 
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+require ('conn.php');
+
+$conn = new Conn();
+$pdo = $conn->connect();
+
+$infosPost = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+  
+if(empty($infosPost['rg-beneficiario'])){
+    header("Location: ../../perda-cartao");
+    echo $_SESSION['error-solicitacao'] = '<div id="erro-todos"
+    class="flex items-center p-4 mb-4 text-red-800 border-t-4 border-red-300 bg-red-50  dark:border-red-800"
+    role="alert">
+    <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+      viewBox="0 0 20 20">
+      <path
+        d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+    </svg>
+    <div class="ms-3 text-sm font-medium">
+      <p class="text-lg md:text-md">RG é obrigatório</p>
+    </div>
+    <button type="button"
+      class="ms-auto -mx-1.5 -my-1.5 bg-red-50 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex items-center justify-center h-8 w-8"
+      data-dismiss-target="#erro-todos" aria-label="Close">
+      <span class="sr-only">Dismiss</span>
+      <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+      </svg>
+    </button>
+    </div>';
+    exit();
+  } 
+  
+  try {
+    $query = "INSERT INTO solicitacao_2a_via (rg_beneficiario, img_bo) VALUES (:rg_beneficiario, :img_bo)";
+    $stmt = $pdo->prepare($query);
+  
+    $executed = $stmt->execute([
+      ":rg_beneficiario" => $infosPost['rg-beneficiario'],
+      ":img_bo" => $infosPost['img-bo'] ?? ""
+    ]);
+  
+    if ($executed) {
+      $_SESSION['success-solicitacao'] = '<div id="alert-border-3" class="flex items-center p-4 mb-4 text-green-800 border-t-4 border-green-300 bg-green-50"
+      role="alert">
+      <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+        viewBox="0 0 20 20">
+        <path
+          d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+      </svg>
+      <div class="ms-3 text-sm font-medium">
+        <p class="text-lg md:text-md">Solicitação enviada com sucesso!</p>
+      </div>
+      <button type="button"
+        class="ms-auto -mx-1.5 -my-1.5 bg-green-50 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex items-center justify-center h-8 w-8"
+        data-dismiss-target="#alert-border-3" aria-label="Close">
+        <span class="sr-only">Dismiss</span>
+        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+        </svg>
+      </button>
+    </div>';
+    }
+  
+    header("Location: ../../perda-cartao");
+    exit();
+  
+  } catch (PDOException $e) {
+    $_SESSION['error-solicitacao'] = "Erro com o banco de dados: " . $e->getMessage();
+    header("Location: ../../perda-cartao");
+    exit();
+  }
