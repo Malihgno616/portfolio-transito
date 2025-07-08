39e15ab73fb58476013b7b090d01d141a59a5328
Ackin.Dev <ackinlino123@gmail.com>
2025-07-08 08:44:16 -0300

    Removendo um arquivo obsoleto

--- form-ait.php
+++ form-ait.php
@@ -5,7 +5,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Formulário AIT</title>
-  <!-- <?php include('../layout/head-format.php');?> -->
   <?php include('layout/head.php');?>
 </head>
 
