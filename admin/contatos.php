768cd8232b13f31239160d68c1c67ff8dbb3d641
Ackin.Dev <ackinlino123@gmail.com>
2025-07-22 09:56:30 -0300

    Criando modal para exibir a mensagem completa

--- admin/contatos.php
+++ admin/contatos.php
@@ -41,15 +41,6 @@ $totalContacts = $data['total'];
 $start = (($currentPage - 1) * 15) + 1;
 $end = min($currentPage * 15, $totalContacts);
 
-// Deletar dados
-// $deleted = $model->deleteContact($_GET['id']);
-
-// if ($deleted) {
-//     header("Location: contacts.php");
-//     exit();
-// }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -74,6 +65,7 @@ include __DIR__.'/layout/header.php';
                       <th scope="col" class="px-6 py-3">Mensagem</th>
                       <th scope="col" class="px-6 py-3">Ações</th> 
                   </tr>
+              </thead>
               <tbody>
                   <?php foreach ($contacts as $contact): ?>
                   <tr class="bg-white border-b  hover:bg-gray-50">
@@ -86,56 +78,82 @@ include __DIR__.'/layout/header.php';
                         </div>  
                       </td>
                       <td class="px-6 py-4 text-lg">
-                          <button  class="font-medium rounded-lg p-1 bg-green-100 text-green-600 dark:text-green-500 hover:bg-green-200">Mensagem</button>
-                          <button  class="font-medium rounded-lg p-1 bg-yellow-100 text-yellow-600 dark:text-yellow-500 hover:bg-yellow-200">Editar</button>
-                          <button  class="font-medium rounded-lg p-1 bg-red-100 text-red-600 dark:text-red-500 hover:bg-red-200">Excluir</button>
+                        <button type="button" data-modal-target="modal-<?= $contact['id'] ?>" data-modal-toggle="modal-<?= $contact['id'] ?>" class="font-medium rounded-lg p-1 bg-green-100 text-green-600 dark:text-green-500 hover:bg-green-200">Mensagem</button>
+                        <button  class="font-medium rounded-lg p-1 bg-yellow-100 text-yellow-600 dark:text-yellow-500 hover:bg-yellow-200">Editar</button>
+                        <button  class="font-medium rounded-lg p-1 bg-red-100 text-red-600 dark:text-red-500 hover:bg-red-200">Excluir</button>
                       </td>
-                  </tr>
-                  <?php endforeach; ?>
-              </tbody>
-              <tfoot>
-                <tr class="bg-gray-50">
-                    <td colspan="5" class="px-6 py-3">
-                        <nav class="flex items-center justify-between pt-2" aria-label="Table navigation">
-                            <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
-                                Mostrando <span class="font-semibold text-gray-900"><?= $start ?>-<?= $end ?></span> 
-                                de <span class="font-semibold text-gray-900"><?= $totalContacts ?></span>
-                            </span>
-                            <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-                                <!-- Link Anterior -->
-                                <li>
-                                    <a href="?page=<?= max(1, $currentPage - 1) ?>" 
-                                    class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 <?= $currentPage == 1 ? 'opacity-50 cursor-not-allowed' : '' ?>">
-                                        Anterior
-                                    </a>
-                                </li>
-
-                                <!-- Links das páginas -->
-                                <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+                    </tr>                    
+                    <?php endforeach; ?>                   
+                </tbody>
+              
+                <tfoot>
+                    <tr class="bg-gray-50">
+                        <td colspan="5" class="px-6 py-3">
+                            <nav class="flex items-center justify-between pt-2" aria-label="Table navigation">
+                                <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+                                    Mostrando <span class="font-semibold text-gray-900"><?= $start ?>-<?= $end ?></span> 
+                                    de <span class="font-semibold text-gray-900"><?= $totalContacts ?></span>
+                                </span>
+                                <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
                                     <li>
-                                        <a href="?page=<?= $i ?>" 
-                                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 <?= $i == $currentPage ? 'text-yellow-600 bg-blue-50 hover:bg-yellow-100 hover:text-yellow-700' : '' ?>">
-                                            <?= $i ?>
+                                        <a href="?page=<?= max(1, $currentPage - 1) ?>" 
+                                        class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 <?= $currentPage == 1 ? 'opacity-50 cursor-not-allowed' : '' ?>">
+                                            Anterior
                                         </a>
                                     </li>
-                                <?php endfor; ?>
-                                
-                                <!-- Link Próxima -->
-                                <li>
-                                    <a href="?page=<?= min($totalPages, $currentPage + 1) ?>" 
-                                    class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 <?= $currentPage == $totalPages ? 'opacity-50 cursor-not-allowed' : '' ?>">
-                                        Próxima
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
-                    </td>
-                </tr>
-            </tfoot>
-          </table>
-      </div>
+
+                                    <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+                                        <li>
+                                            <a href="?page=<?= $i ?>" 
+                                            class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 <?= $i == $currentPage ? 'text-yellow-600 bg-blue-50 hover:bg-yellow-100 hover:text-yellow-700' : '' ?>">
+                                                <?= $i ?>
+                                            </a>
+                                        </li>
+                                    <?php endfor; ?>
+
+                                    <li>
+                                        <a href="?page=<?= min($totalPages, $currentPage + 1) ?>" 
+                                        class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 <?= $currentPage == $totalPages ? 'opacity-50 cursor-not-allowed' : '' ?>">
+                                            Próxima
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </td>
+                    </tr>
+                </tfoot>
+        </table>
     </div>
-  </div>
 </main>
+
+<?php foreach($contacts as $contact): ?>
+<div id="modal-<?= $contact['id'] ?>" tabindex="-1" 
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-[9999] justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full animate__animated animate__fadeInDown">
+
+    <div class="relative p-4 w-full max-w-2xl max-h-full">
+        <div class="relative bg-white rounded-lg shadow-xl">
+
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t border-gray-200">
+                <h3 class="text-xl font-light text-gray-900">
+                    Mensagem de: <strong><?= htmlspecialchars($contact['nome']) ?></strong>
+                </h3>
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="modal-<?= $contact['id'] ?>">
+                    <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Fechar modal</span>
+                </button>
+            </div>
+
+            <div class="p-4 md:p-5 space-y-4">
+                <p class="text-lg text-justify text-gray-700">
+                    <?= htmlspecialchars($contact['mensagem']) ?>
+                </p>
+            </div>
+
+        </div>
+    </div>
+</div>
+<?php endforeach; ?>
 
 <?php include __DIR__.'/layout/footer.php';?> 
