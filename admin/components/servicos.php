c38f88e85fd58ca8c3fcf3d0e871d8398589004a
Ackin.Dev <ackinlino123@gmail.com>
2025-10-02 10:59:44 -0300

    Mudando a visualização dos boxes

--- admin/components/servicos.php
+++ admin/components/servicos.php
@@ -2,14 +2,25 @@
   <div class="flex justify-center animate__animated animate__fadeIn">
     <div class="p-10 w-full">
       <div class="flex items-center justify-center flex-col gap-7">
-          <a href="tab-idoso.php" class="text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-300/90 p-10 hover:shadow-xl duration-150">
-            <i class="fa-solid fa-person-cane"></i>
-            Cartões do Idoso
+          <a href="tab-idoso.php" class="m-auto text-center h-auto w-[35em] rounded-md bg-gray-200 p-10 hover:shadow-xl duration-150">
+            <div class="flex flex-col items-center justify-center">
+              <span class="text-3xl p-2">Cartões do Idoso</span>
+              <span class="font-light text-2xl">Visualize e gerencie os cartões do idoso</span>
+              <i class="text-4xl text-yellow-600 fa-solid fa-person-cane"></i>
+            </div>
           </a>
-          <a href="tab-deficiente.php" class="text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-300/90 p-10 hover:shadow-xl duration-150">
-            <i class="fa-solid fa-wheelchair"></i>
-            Cartões do Deficiente
+          <a href="tab-deficiente.php" class="m-auto text-center h-auto w-[35em] rounded-md bg-gray-200 p-10 hover:shadow-xl duration-150">
+            <div class="flex flex-col items-center justify-center">
+              <span class="text-3xl p-2">Cartões do Deficiente</span>
+              <span class="font-light text-2xl">Visualize e gerencie os cartões do deficiente</span>
+              <i class="text-3xl text-yellow-600 fa-solid fa-wheelchair"></i>
+            </div>
           </a>
       </div>
+      <div class="m-auto text-center p-12">
+        <a href="home.php" class="text-3xl w-32 p-2 rounded-xl bg-yellow-600 text-white hover:bg-yellow-500 duration-75">Voltar</a>
+      </div>
     </div>
+    
+  
   </div>
