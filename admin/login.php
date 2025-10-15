bf54f8631ce4c1f3e1130bbdc06a498d7b8fb6c2
Ackin.Dev <ackinlino123@gmail.com>
2025-10-15 09:46:04 -0300

    Adicionando a imagem comprimida na tela de login

--- admin/login.php
+++ admin/login.php
@@ -41,7 +41,7 @@ if(!in_array($current_page, $allowed_pages)) {
 <html lang="pt-br">
 <?php include __DIR__.'/components/head.php'; ?>
 <body>
-  <div class="bg-gray-500 bg-[url(assets/img/697.jpg)] h-screen bg-center bg-no-repeat bg-cover bg-blend-multiply">
+  <div class="bg-gray-500 bg-[url(assets/img/697-compressed.jpg)] h-screen bg-center bg-no-repeat bg-cover bg-blend-multiply">
     <?php require __DIR__.'/components/form-login.php';?>
   </div>
   <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
