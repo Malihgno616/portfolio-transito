f95b15d1a511dc1a317ecf9cd46f3750de77fe81
Ackin.Dev <ackinlino123@gmail.com>
2025-10-20 10:23:41 -0300

    Mudando os estilos dos cards no home do site administrativo

--- admin/components/home.php
+++ admin/components/home.php
@@ -3,26 +3,26 @@
         <h1 class="text-5xl text-center mb-5">Administrativo - Início</h1>
         <hr>
         <br>
-        <div class="flex items-center justify-center flex-col gap-7 font-light">
-            <a href="servicos.php" class="text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
-              <i class="text-yellow-600 fa-solid fa-briefcase"></i>  
+        <div class="flex items-center justify-center flex-col gap-7 font-normal">
+            <a href="servicos.php" class="text-gray-700 flex flex-col text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
               Serviços
+              <p class="text-xl p-2 font-medium text-gray-500">Serviços Online</p>
+              <i class="text-yellow-600 fa-solid fa-briefcase text-4xl"></i>  
             </a>
-            <a href="contatos.php" class="text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
-              <i class="text-yellow-600 fa-solid fa-phone"></i>
-              Contatos
-            </a>
-            <a href="#" class="text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
-              <i class="text-yellow-600 fa-solid fa-bell"></i>
-              Notificações
-            </a>
-            <a href="noticias.php" class="text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
-              <i class="text-yellow-600 fa-solid fa-newspaper"></i>
-              Notícias
-            </a>
-            <a href="usuarios.php" class="text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
-              <i class="text-yellow-600 fa-solid fa-user-tie"></i>
+            <a href="usuarios.php" class="text-gray-700 flex flex-col text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
               Usuários
+              <p class="text-xl p-2 font-medium text-gray-500">Gerencie os Usuários</p>
+              <i class="text-yellow-600 fa-solid fa-user-tie text-4xl"></i>
+            </a>
+            <a href="contatos.php" class="text-gray-700 flex flex-col text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
+              Contatos
+              <p class="text-xl p-2 font-medium text-gray-500">Tabela de contato</p>
+              <i class="text-yellow-600 fa-solid fa-phone text-4xl"></i>
+            </a>
+            <a href="#" class="text-gray-700 flex flex-col text-3xl m-auto text-center h-auto w-120 rounded-lg bg-gray-200 p-10 hover:shadow-xl duration-150">
+              Slider
+              <p class="text-xl p-2 font-medium text-gray-500">Edite os Sliders do site principal</p>
+              <i class="text-yellow-600 fa-solid fa-images text-4xl"></i>
             </a>
         </div>
       </div>
