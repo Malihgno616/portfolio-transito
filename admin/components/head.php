26ff2add5725675e8cd6440f0f78162df851acc5
Ackin.Dev <ackinlino123@gmail.com>
2025-11-18 11:07:13 -0300

    teste

--- admin/components/head.php
+++ admin/components/head.php
@@ -4,8 +4,8 @@
   <title>Painel de Controle - Trânsito Leme</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="/node_modules/flowbite/dist/flowbite.min.js"></script>
-  <link rel="stylesheet" href="assets/css/global.css" />
-  <link rel="stylesheet" href="src/output.css" />
+  <link rel="stylesheet" href="/assets/css/global.css" />
+  <link rel="stylesheet" href="/src/output.css" />
   <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
   <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css" />
