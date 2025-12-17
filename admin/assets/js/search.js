2fedf3aa525fd76f8ee4c644b8b16a427bad2c25
Ackin.Dev <ackinlino123@gmail.com>
2025-12-17 13:59:30 -0300

    Exibindo os dados da api via Axios (correção)

--- admin/assets/js/search.js
+++ admin/assets/js/search.js
@@ -1,12 +1,163 @@
-axios
-  .get("http://localhost/portfolio-transito/admin/search.php", {
-    params: {
-      type: "contact",
-      name: "A",
-    },
-  })
-  .then(function (response) {
-    console.log(response.data);
+// Evento para pesquisa de contatos
+document.addEventListener("DOMContentLoaded", () => {
+  const inputIdContact = document.getElementById("input-contact-id");
+
+  const inputNameContact = document.getElementById("input-contact-name");
+
+  const inputPhoneContact = document.getElementById("input-contact-phone");
+
+  const tableBodyContact = document.getElementById("table-contact-body");
+
+  tableBodyContact.classList.add(
+    "w-full",
+    "text-sm",
+    "text-left",
+    "rtl:text-right",
+    "text-gray-500",
+    "dark:text-gray-800"
+  );
+
+  inputNameContact.addEventListener("input", async (e) => {
+    const query = e.target.value;
+
+    try {
+      const response = await axios.get(
+        "http://localhost/portfolio-transito/admin/search.php",
+        {
+          params: {
+            type: "contact",
+            id: "",
+            name: query,
+            phone: "",
+          },
+        }
+      );
+      tableBodyContact.innerHTML = `
+        <thead class="text-lg text-gray-700 uppercase bg-gray-50 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">ID</th>
+                <th scope="col" class="px-6 py-3">Nome</th>
+                <th scope="col" class="px-6 py-3">Email</th>
+                <th scope="col" class="px-6 py-3">Telefone</th>
+                <th scope="col" class="px-6 py-3">Ações</th> 
+            </tr>
+        </thead>
+        <tbody>
+             <tr class="bg-white border-b  hover:bg-gray-50">
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.id || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.nome || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.email || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.telefone || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg"></td>
+             </tr>
+        </tbody>
+      `;
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
   });
 
+  inputIdContact.addEventListener("input", async (e) => {
+    const query = e.target.value;
 
+    try {
+      const response = await axios.get(
+        "http://localhost/portfolio-transito/admin/search.php",
+        {
+          params: {
+            type: "contact",
+            id: query,
+            name: "",
+            phone: "",
+          },
+        }
+      );
+      tableBodyContact.innerHTML = `
+        <thead class="text-lg text-gray-700 uppercase bg-gray-50 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">ID</th>
+                <th scope="col" class="px-6 py-3">Nome</th>
+                <th scope="col" class="px-6 py-3">Email</th>
+                <th scope="col" class="px-6 py-3">Telefone</th>
+                <th scope="col" class="px-6 py-3">Ações</th> 
+            </tr>
+        </thead>
+        <tbody>
+             <tr class="bg-white border-b  hover:bg-gray-50">
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.id || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.nome || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.email || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.telefone || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg"></td>
+             </tr>
+        </tbody>
+      `;
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+  });
+
+  inputPhoneContact.addEventListener("input", async (e) => {
+    const query = e.target.value;
+
+    try {
+      const response = await axios.get(
+        "http://localhost/portfolio-transito/admin/search.php",
+        {
+          params: {
+            type: "contact",
+            id: "",
+            name: "",
+            phone: query,
+          },
+        }
+      );
+      tableBodyContact.innerHTML = `
+        <thead class="text-lg text-gray-700 uppercase bg-gray-50 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">ID</th>
+                <th scope="col" class="px-6 py-3">Nome</th>
+                <th scope="col" class="px-6 py-3">Email</th>
+                <th scope="col" class="px-6 py-3">Telefone</th>
+                <th scope="col" class="px-6 py-3">Ações</th> 
+            </tr>
+        </thead>
+        <tbody>
+             <tr class="bg-white border-b  hover:bg-gray-50">
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.id || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.nome || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.email || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg">${
+                  response.telefone || ""
+                }</td>
+                <td class="px-6 py-4 font-bold text-lg"></td>
+             </tr>
+        </tbody>
+      `;
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+  });
+});
--- admin/components/modal-pesquisa-contato.php
+++ admin/components/modal-pesquisa-contato.php
@@ -10,14 +10,39 @@
                     <span class="sr-only">Close modal</span>
                 </button>
             </div>
-            <form class="max-w-4xl mx-auto m-10">   
-                <label for="search" class="block mb-2.5 text-sm font-medium text-heading sr-only ">Search</label>
+            <form class="max-w-4xl mx-auto m-10 flex flex-col gap-6">   
                 <div class="relative">
+                    <label for="search" class="block mb-2.5 text-sm font-medium text-heading sr-only ">Search</label>
                     <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/></svg>
                     </div>
-                    <input type="search" id="search-contato" class="text-lg block w-full p-3 ps-9 bg-neutral-secondary-medium border border-gray-200 text-heading rounded-lg focus:ring-yellow-400 focus:border-yellow-600 shadow-xs placeholder:text-body" />
+                    <input type="search" id="input-contact-id"
+                    placeholder="Pesquisar pelo Id"
+                    class="text-lg block w-full p-3 ps-9 bg-neutral-secondary-medium border border-gray-200 text-heading rounded-lg focus:ring-yellow-400 focus:border-yellow-600 shadow-xs placeholder:text-body" />  
                 </div>
+
+                <div class="relative">
+                    <label for="search" class="block mb-2.5 text-sm font-medium text-heading sr-only ">Search</label>
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/></svg>
+                    </div>
+                    <input type="search" id="input-contact-name"
+                    placeholder="Pesquisar pelo nome"
+                    class="text-lg block w-full p-3 ps-9 bg-neutral-secondary-medium border border-gray-200 text-heading rounded-lg focus:ring-yellow-400 focus:border-yellow-600 shadow-xs placeholder:text-body" />
+                </div>
+
+                <div class="relative">
+                    <label for="search" class="block mb-2.5 text-sm font-medium text-heading sr-only ">Search</label>
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-body" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/></svg>
+                    </div>
+                    <input type="search" id="input-contact-phone"
+                    placeholder="Pesquisar pelo nº de telefone"
+                    class="text-lg block w-full p-3 ps-9 bg-neutral-secondary-medium border border-gray-200 text-heading rounded-lg focus:ring-yellow-400 focus:border-yellow-600 shadow-xs placeholder:text-body" />
+                </div>
+ 
+                <div id="table-contact-body"></div>
+
             </form>
         </div>
     </div>
