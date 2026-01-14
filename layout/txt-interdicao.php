3241a5f40f871e25a00b4577b9bf69fed0f0b9f4
Ackin.Dev <ackinlino123@gmail.com>
2026-01-14 12:00:45 -0300

    atualizando a página de interdições

--- layout/txt-interdicao.php
+++ layout/txt-interdicao.php
@@ -1,31 +1,76 @@
-<main class="texto-format animate__animated animate__fadeIn mx-auto p-5 sm:p-10">
+<main class="animate__animated animate__fadeIn mx-auto p-5 sm:p-10">
 
   <h1 class="text-4xl sm:text-5xl p-5">Interdições</h1>
-  <p class="text-justify text-lg sm:text-xl p-5">Para solicitar a interdição de via, deverá ser preenchido o
-    <strong>Requerimento para Interdição de Via Pública</strong></p>
+  <p class="text-justify text-lg sm:text-xl p-5"><u>O que eu preciso para solicitar o serviço de interdição de via pública?</u></p>
+  <p class="text-justify text-lg sm:text-xl p-5">
+    Formulário de <strong>Requerimento de Interdição</strong> e Formulário <strong>Abaixo Assinado</strong> impresso, preenchido e assinado.
+  </p>
+
+  <p class="text-justify text-lg sm:text-xl p-5">
+    <u>No Requerimento de Interdição deverá ser informado:</u>
+  </p>
+
+  <p class="text-justify text-lg sm:text-xl p-5">
+    - Dados do responsável e detalhes do evento (tipo, atividades que serão desenvolvidas, características, público estimado, trecho a bloquear ou percurso, indicação no mapa, data e horário).
+  </p>
+
+  <p class="text-justify text-lg sm:text-xl p-5">
+    <u>
+    No Requerimento de Abaixo Assinado deverá:
+    </u>
+  </p>
+
+  <p class="text-justify text-lg sm:text-xl p-5">
+    - Preencher com nome, RG, número da residência e assinatura dos moradores do trecho que será interditado. Se for edifício, apenas o preenchimento pelo síndico.
+  </p>
+
+  <p class="text-justify text-lg sm:text-xl p-5">
+    <u>
+      Dependendo da característica do evento, também podem ser exigidos:
+    </u>
+  </p>
+
+  <p class="text-justify text-lg sm:text-xl">
+    <ul class="text-justify text-lg sm:text-xl p-5">
+      <li>- Autorização da Secretaria Municipal de Cultura;
+      <li>- Autorização da Secretaria Municipal de Esporte e Lazer;
+      <li>- Autorização do Núcleo de Posturas;
+    </ul>
+  </p>
 
   <section class="flex flex-wrap sm:flex-col items-start gap-4 mb-7">
-    <p class="text-base sm:text-sm px-5 sm:px-0 sm:text-center">Para baixar o requerimento: </p>
-    <a class="bg-yellow-500 ml-4 px-4 py-2 rounded-md hover:bg-amber-200 duration-200 text-lg" href="#" rel="noopener noreferrer">clique aqui</a>
-    <p class="text-base sm:text-sm px-5 sm:px-0 sm:text-center">
-      Qualquer dúvida entre em contato com este e-mail:
-    </p>
-    <a class="bg-yellow-500 ml-4 px-4 py-2 rounded-md hover:bg-amber-200 duration-200 text-lg"
-      href="mailto:nucledetransito@leme.sp.gov.br">nucledetransito@leme.sp.gov.br</a>
-    <p class="text-base sm:text-sm px-5 sm:px-0 sm:text-center">
-      Ou preencha o formulário de contato:
-    </p>
-    <a class="bg-yellow-500 ml-4 px-4 py-2 rounded-md hover:bg-amber-200 duration-200 text-lg"
-      href="contato">Formulário de contato</a>
+    
+    <p class="text-lg sm:text-sm px-5 sm:px-0 sm:text-center">Para baixar o Requerimento de Interdição de Via Pública: </p>
+    <a class="bg-yellow-500 ml-4 rounded-md p-1 hover:bg-amber-200 duration-200 text-lg" href="#" rel="noopener noreferrer">clique aqui</a>
+
+    <p class="text-lg sm:text-sm px-5 sm:px-0 sm:text-center">Para baixar o Formulário de Abaixo Assinado: </p>
+    <a class="bg-yellow-500 ml-4 rounded-md p-1 hover:bg-amber-200 duration-200 text-lg" href="#" rel="noopener noreferrer">clique aqui</a>
+  
   </section>
 
-  <section class="flex flex-wrap sm:flex-col items-start gap-4 mb-7">
-    <p class="text-justify text-lg sm:text-sm px-5 sm:px-0 sm:text-center">Preencher todos os campos do requerimento</p>
-    <p class="text-justify text-lg sm:text-sm px-5 sm:px-0 ">O protocolo não caracteriza que o requerimento foi
-      DEFERIDO, somente terá efeito com a expedição da AUTORIZAÇÃO ao requerente, que deverá ser retirado no Núcleo de
-      Trânsito, de segunda à sexta-feira das 08h às 16h.</p>
-    <p class="text-justify text-lg sm:text-sm px-5 sm:px-0 sm:text-center">A interdição e a liberação da via será
-      realizada no horário deste requerimento pelos funcionários do setor, NÃO PODENDO SER ESTENDIDO.</p>
-  </section>
+  <p class="text-justify text-lg sm:text-xl p-5">
+    <u>Como solicito esse serviço?</u>
+  </p>
+
+  <p class="text-justify text-lg sm:text-xl">
+    <ul class="flex flex-col gap-3 items-left justify-center text-justify text-lg sm:text-xl p-5">
+      <li>
+        1. Imprima, preencha e assine o formulário de Interdição de Via Pública para realização do evento;
+      </li>
+      <li>
+        2. Imprima e colha as assinaturas do Abaixo-assinado dos moradores da via/quadra a ser fechada. Se for edifício, apenas o síndico assina;
+      </li>
+      <li>
+        3. Digitalize o formulario de interdição, o abaixo-assinado e toda a documentação exigida e realize o protocolo com no mínimo 10 (dez) dias úteis de antecedência, presencialmente no protocolo da Prefeitura ou digitalmente através do link abaixo, pelo sistema 1doc, escolhendo o assunto Interdição de Via: <a class="bg-yellow-500 ml-1 rounded-md p-1 hover:bg-amber-200 duration-200 text-lg" href="#" rel="noopener noreferrer">Protocolo Digital 1Doc</a>
+      </li>
+      <li>
+        4. Aguarde a resposta da Coordenadoria de Trânsito por telefone ou e-mail. Se o evento for autorizado, a autorização deverá ser acessada e impressa pelo organizador;
+      </li>
+      <li>
+        5. Realize o evento no local, data e horário definidos.
+      </li>
+    </ul>
+  </p>
+  
 
 </main>
