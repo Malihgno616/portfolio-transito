ad6f999e4515b31f9275a01b9cdb84c6edae465f
Ackin.Dev <ackinlino123@gmail.com>
2026-01-15 10:20:36 -0300

    Mudança do layout do footer no site administrativo

--- admin/components/footer.php
+++ admin/components/footer.php
@@ -3,10 +3,9 @@
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="home.php" class="flex items-center mb-2 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <img src="assets/img/logo-borda-branca.png" class="h-20" alt="Flowbite Logo" />
+                <span class="italic text-3xl text-white">Administrativo</span>
             </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-white">
-                <span class="italic text-3xl text-white">Painel de Controle</span>
-            </ul>
+            <a href="../" class="hover:underline">Voltar para a Página Principal</a>
         </div>
         <hr class="my-4 border-1 rounded-2xl border-gray-400/50 w-250 sm:mx-auto dark:border-gray-900 lg:my-8" />
         <span class="block text-lg text-gray-500 sm:text-center dark:text-black">©<?= date("Y") ?> <a href="home.php" class="hover:underline">Trânsito Leme</a>. Todos os direitos reservados.</span>
