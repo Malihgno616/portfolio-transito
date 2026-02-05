12d30fca9fe5b2be8816ece003dfc2c29cdaafd4
Ackin.Dev <ackinlino123@gmail.com>
2026-02-05 11:27:35 -0300

    fix: Correção na atualização do cartão do deficiente

--- config/database/edit-card-deficiente-db.php
+++ config/database/edit-card-deficiente-db.php
@@ -1,120 +1,44 @@
-<?php 
-
+<?php
 session_start();
 
-require('conn.php');
+require_once __DIR__ . '/../../models/FormDeficiente.php';
+
+use Models\FormDeficiente;
+
+function setAlert($message, $type = 'success') {
+    $colorClass = $type === 'success' 
+        ? 'text-green-800 bg-green-50' 
+        : 'text-red-800 bg-red-50';
+    
+    return <<<HTML
+    <div id="alert-3" class="flex items-center p-4 mb-4 rounded-lg $colorClass w-15" role="alert">
+        <svg class="shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+        </svg>
+        <div class="ms-3 text-lg font-medium">
+            $message 
+        </div>
+        <button type="button" class="ms-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 p-1.5 hover:bg-opacity-80 inline-flex items-center justify-center h-8 w-8" data-dismiss-target="#alert-3" aria-label="Close">
+            <span class="sr-only">Close</span>
+            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+            </svg>
+        </button>
+    </div>
+    HTML;  
+}
 
 $infos = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 $files = $_FILES;
 
-$conn = new Conn();
-$pdo = $conn->connect();
+$model = new FormDeficiente();
+$updated = $model->updateDeficiente($infos, $files);
 
-try {
-    $querySelect = "SELECT * FROM cartao_deficiente WHERE rg_beneficiario = ?";
-    $stmtSelect = $pdo->prepare($querySelect);
-    $stmtSelect->execute([$infos['rg-beneficiario']]);
-    $currentData = $stmtSelect->fetch(PDO::FETCH_ASSOC);
-
-    $setClauses = [];
-    $params = [];
-    $types = [];
-
-    $fields = [
-        'nome_beneficiario' => $infos['nome-beneficiario'],
-        'nasc_beneficiario' => $infos['nascimento-beneficiario'],
-        'genero_beneficiario' => $infos['genero-beneficiario'],
-        'endereco_beneficiario' => $infos['endereco-beneficiario'],
-        'numero_beneficiario' => $infos['numero-beneficiario'],
-        'complemento_beneficiario' => $infos['complemento-beneficiario'],
-        'bairro_beneficiario' => $infos['bairro-beneficiario'],
-        'cep_beneficiario' => $infos['cep-beneficiario'],
-        'cidade_beneficiario' => $infos['cidade-beneficiario'],
-        'uf_beneficiario' => $infos['uf-beneficiario'],
-        'telefone_beneficiario' => $infos['telefone-beneficiario'],
-        'rg_beneficiario' => $infos['rg-beneficiario'],
-        'expedicao_beneficiario' => $infos['expedicao-beneficiario'],
-        'expedido_beneficiario' => $infos['expedido-beneficiario'],
-        'cnh_beneficiario' => $infos['cnh-beneficiario'],
-        'validade_cnh_beneficiario' => $infos['validade-cnh-beneficiario'],
-        'email_beneficiario' => $infos['email-beneficiario'],
-        'nome_medico' => $infos['nome-medico'],
-        'crm' => $infos['crm-medico'],
-        'telefone_medico' => $infos['telefone-medico'],
-        'local_atendimento_medico' => $infos['local-atendimento-medico'],
-        'deficiencia_ambulatoria' => isset($infos['deficiencia-ambulatoria']) ? implode(',', $infos['deficiencia-ambulatoria']) : null,
-        'periodo_restricao_medica' => $infos['restricao-medica'],
-        'data_inicio' => $infos['data-inicio'],
-        'data_fim' => $infos['data-fim'],
-        'cid' => $infos['cid'],
-        'nome_representante' => $infos['nome-representante'],
-        'email_representante' => $infos['email-representante'],
-        'endereco_representante' => $infos['endereco-representante'],
-        'num_representante' => $infos['numero-representante'],
-        'complemento_representante' => $infos['complemento-representante'],
-        'bairro_representante' => $infos['bairro-representante'],
-        'cep_representante' => $infos['cep-representante'],
-        'cidade_representante' => $infos['cidade-representante'],
-        'uf_representante' => $infos['uf-representante'],
-        'telefone_representante' => $infos['telefone-representante'],
-        'rg_representante' => $infos['rg-representante'],
-        'expedicao_representante' => $infos['expedicao-representante'],
-        'expedido_representante' => $infos['expedido-representante']
-    ];
-
-    $blobFields = [
-        'copia_rg_beneficiario' => 'copia-rg-beneficiario',
-        'atestado_medico' => 'atestado-medico',
-        'copia_rg_representante' => 'copia-rg-representante',
-        'comprovante_representante' => 'comprovante-representante'
-    ];
-
-    foreach ($blobFields as $dbField => $fileField) {
-        if (!empty($files[$fileField]['name'])) {
-            $fileContent = file_get_contents($files[$fileField]['tmp_name']);
-            $fields[$dbField] = $fileContent;
-        } else {
-            $fields[$dbField] = $currentData[$dbField];
-        }
-    }
-
-    foreach ($fields as $field => $value) {
-        if ($value !== null && $value !== 'Não possui') {
-            $setClauses[] = "$field = ?";
-            $params[] = $value;
-            $types[] = (in_array($field, array_keys($blobFields))) ? PDO::PARAM_LOB : PDO::PARAM_STR;
-        }
-    }
-    
-    $params[] = $infos['rg-beneficiario'];
-
-    if (empty($setClauses)) {
-        throw new Exception("Nenhum campo válido para atualizar.");
-    }
-
-    $query = "UPDATE cartao_deficiente SET " . implode(', ', $setClauses) . " WHERE rg_beneficiario = ?";
-    $stmt = $pdo->prepare($query);
-
-    foreach ($params as $index => $value) {
-        $type = isset($types[$index]) ? $types[$index] : PDO::PARAM_STR;
-        $stmt->bindValue($index + 1, $value, $type);
-    }
-
-    $executed = $stmt->execute();
-
-    if ($executed) {
-        $_SESSION['success-renova-card-def'] = "Informações alteradas com sucesso";
-    } else {
-        $_SESSION['err-renova-card-def'] = "Erro ao alterar os dados";
-    }
-    
-    header("Location: ../../renovar-cartao.php");
-    exit();
-  
-} catch (PDOException $e) {
-    echo "Erro ao conectar ao banco de dados: " . $e->getMessage();
-} catch (Exception $e) {
-    $_SESSION['err-renova-card-def'] = $e->getMessage();
-    header("Location: ../../renovar-cartao.php");
-    exit();
+if ($updated) {
+    $_SESSION['renova-alert'] = setAlert("Dados alterados com sucesso!", 'success');
+} else {
+    $_SESSION['renova-alert'] = setAlert("Erro ao alterar os dados", 'error');
 }
+
+header("Location: ../../renovar-cartao.php");
+exit();
--- models/FormDeficiente.php
+++ models/FormDeficiente.php
@@ -185,6 +185,114 @@ class FormDeficiente {
         }
     }
 
+    public function updateDeficiente(array $infos, array $files)
+    {
+        try {
+            $querySelect = "SELECT * FROM cartao_deficiente WHERE rg_beneficiario = ?";
+            $stmtSelect = $this->pdo->prepare($querySelect);
+            $stmtSelect->execute([$infos['rg-beneficiario']]);
+            $currentData = $stmtSelect->fetch(PDO::FETCH_ASSOC);
+
+            if (!$currentData) {
+                throw new \Exception("RG não encontrado.");
+            }
+
+            $setClauses = [];
+            $params = [];
+            $types = [];
+
+            $fields = [
+                'nome_beneficiario' => $infos['nome-beneficiario'],
+                'nasc_beneficiario' => $infos['nascimento-beneficiario'],
+                'genero_beneficiario' => $infos['genero-beneficiario'],
+                'endereco_beneficiario' => $infos['endereco-beneficiario'],
+                'numero_beneficiario' => $infos['numero-beneficiario'],
+                'complemento_beneficiario' => $infos['complemento-beneficiario'],
+                'bairro_beneficiario' => $infos['bairro-beneficiario'],
+                'cep_beneficiario' => $infos['cep-beneficiario'],
+                'cidade_beneficiario' => $infos['cidade-beneficiario'],
+                'uf_beneficiario' => $infos['uf-beneficiario'],
+                'telefone_beneficiario' => $infos['telefone-beneficiario'],
+                'rg_beneficiario' => $infos['rg-beneficiario'],
+                'expedicao_beneficiario' => $infos['expedicao-beneficiario'],
+                'expedido_beneficiario' => $infos['expedido-beneficiario'],
+                'cnh_beneficiario' => $infos['cnh-beneficiario'],
+                'validade_cnh_beneficiario' => $infos['validade-cnh-beneficiario'],
+                'email_beneficiario' => $infos['email-beneficiario'],
+                'nome_medico' => $infos['nome-medico'],
+                'crm' => $infos['crm-medico'],
+                'telefone_medico' => $infos['telefone-medico'],
+                'local_atendimento_medico' => $infos['local-atendimento-medico'],
+                'deficiencia_ambulatoria' => isset($infos['deficiencia-ambulatoria']) ? implode(',', $infos['deficiencia-ambulatoria']) : null,
+                'periodo_restricao_medica' => $infos['restricao-medica'],
+                'data_inicio' => $infos['data-inicio'],
+                'data_fim' => $infos['data-fim'],
+                'cid' => $infos['cid'],
+                'nome_representante' => $infos['nome-representante'],
+                'email_representante' => $infos['email-representante'],
+                'endereco_representante' => $infos['endereco-representante'],
+                'num_representante' => $infos['numero-representante'],
+                'complemento_representante' => $infos['complemento-representante'],
+                'bairro_representante' => $infos['bairro-representante'],
+                'cep_representante' => $infos['cep-representante'],
+                'cidade_representante' => $infos['cidade-representante'],
+                'uf_representante' => $infos['uf-representante'],
+                'telefone_representante' => $infos['telefone-representante'],
+                'rg_representante' => $infos['rg-representante'],
+                'expedicao_representante' => $infos['expedicao-representante'],
+                'expedido_representante' => $infos['expedido-representante']
+            ];
+
+            $blobFields = [
+                'copia_rg_beneficiario' => 'copia-rg-beneficiario',
+                'atestado_medico' => 'atestado-medico',
+                'copia_rg_representante' => 'copia-rg-representante',
+                'comprovante_representante' => 'comprovante-representante'
+            ];
+
+            foreach ($blobFields as $dbField => $fileField) {
+                if (!empty($files[$fileField]['name'])) {
+                    $fileContent = file_get_contents($files[$fileField]['tmp_name']);
+                    $fields[$dbField] = $fileContent;
+                } else {
+                    $fields[$dbField] = $currentData[$dbField];
+                }
+            }
+
+            foreach ($fields as $field => $value) {
+                if ($value !== null && $value !== 'Não possui') {
+                    $setClauses[] = "$field = ?";
+                    $params[] = $value;
+                    $types[] = (in_array($field, array_keys($blobFields))) ? PDO::PARAM_LOB : PDO::PARAM_STR;
+                }
+            }
+
+            $params[] = $infos['rg-beneficiario'];
+
+            if (empty($setClauses)) {
+                throw new \Exception("Nenhum campo válido para atualizar.");
+            }
+
+            $query = "UPDATE cartao_deficiente SET " . implode(', ', $setClauses) . " WHERE rg_beneficiario = ?";
+            $stmt = $this->pdo->prepare($query);
+
+            foreach ($params as $index => $value) {
+                $type = isset($types[$index]) ? $types[$index] : PDO::PARAM_STR;
+                $stmt->bindValue($index + 1, $value, $type);
+            }
+
+            return $stmt->execute();
+
+        } catch (PDOException $e) {
+            error_log("Erro ao atualizar: " . $e->getMessage());
+            return false;
+        } catch (\Exception $e) {
+            error_log("Erro ao atualizar: " . $e->getMessage());
+            return false;
+        }
+    }
+
+
     public function lastInsertId()
     {
         try {
