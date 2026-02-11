89f1cc3cf446ca63ae28700c741b92106999f76d
Ackin.Dev <ackinlino123@gmail.com>
2026-02-11 11:57:59 -0300

    refazendo o layout do detalhe da notícia

--- layout/detail-news.php
+++ layout/detail-news.php
@@ -3,21 +3,22 @@
     <div class="p-10">
         <h1 class="text-white text-center text-3xl md:text-4xl lg:text-5xl mb-4">Detalhe da Notícia</h1>
     </div>
-    <div class="animate__animated animate__fadeInDown bg-gray-100 rounded-md p-5 flex flex-col items-center justify-center gap-3">
+    <div class="animate__animated animate__fadeIn bg-white rounded-sm max-w-4xl p-5 flex flex-col items-center justify-center gap-3 m-auto">
         <div class="w-full">
-            <img class="animate__animated animate__fadeIn w-72 p-2 m-auto" src="display-news-img?id=<?= $noticiaDetalhe['id_noticia']?>&type=main" alt="Img notícia principal">
-            <h1 class="text-5xl font-light mb-3 text-center"><?= $noticiaDetalhe['titulo_principal']?></h1>
-            <h2 class="text-3xl font-light mb-2 text-center"><?= $noticiaDetalhe['subtitulo_principal']?></h2>
-            <hr>
+            <img class="shadow-lg rounded-md w-xl sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 h-auto p-2 m-auto" src="display-news-img?id=<?= $noticiaDetalhe['id_noticia']?>&type=main" alt="Img notícia principal">
+            <h1 class="text-5xl mt-5 mb-3 text-center"><?= $noticiaDetalhe['titulo_principal']?></h1>
+            <h2 class="text-3xl mb-2 text-center"><?= $noticiaDetalhe['subtitulo_principal']?></h2>
+            <br>
+            <hr class="text-gray-200">
+            <br>
         </div>
         <div class="w-full">
-            <img class="animate__animated animate__fadeIn w-72 p-2 m-auto" src="display-news-img?id=<?= $noticiaDetalhe['id_noticia']?>&type=content" alt="Img conteúdo da notícia">
-            <h1 class="text-5xl mb-3 text-center"><?= $noticiaDetalhe['titulo_conteudo']?></h1>
-            <h2 class="text-3xl mb-2 text-center"><?= $noticiaDetalhe['subtitulo_conteudo']?></h2>
+            <img class="shadow-lg rounded-md w-xl sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 h-auto p-2 m-auto" src="display-news-img?id=<?= $noticiaDetalhe['id_noticia']?>&type=content" alt="Img conteúdo da notícia">
+            <h2 class="text-3xl mt-3 mb-3 text-center"><?= $noticiaDetalhe['titulo_conteudo']?></h1>
+            <h3 class="text-2xl mb-2 text-center"><?= $noticiaDetalhe['subtitulo_conteudo']?></h2>
             <div class="w-46 p-5 m-auto flex justify-center">
                 <p class="text-justify text-xl"><?= $noticiaDetalhe['texto']?></p>
             </div>
-            <hr>
         </div>
     </div>
     <div class="m-10 flex justify-center ">
