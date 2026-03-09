d78b6af7f3c2fac638e034022b53a897b8b2895e
Ackin.Dev <ackinlino123@gmail.com>
2026-03-09 13:57:27 -0300

    adicionando a lib quill para o site principal

--- layout/head.php
+++ layout/head.php
@@ -6,3 +6,5 @@
   crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="shortcut icon" href="./assets/img/favicon.png" type="image/x-icon">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+<script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css" rel="stylesheet">
