e65576443b285cb49fcd0a3dc4f88b5b1d95bbc5
Ackin.Dev <ackinlino123@gmail.com>
2026-05-20 08:40:59 -0300

    Ajuste no título do form do deficiente

--- admin/form-add-deficiente.php
+++ admin/form-add-deficiente.php
@@ -19,7 +19,7 @@ include __DIR__.'/layout/header.php';
 
   <main class="w-full h-full p-10">
     <div class="w-2xl gap-3 flex flex-col items-center m-auto">
-      <h1 class="text-3xl text-center">Informações do beneficiário</h1>
+      <h1 class="text-3xl text-center">Informações do beneficiário - Registre os dados</h1>
       <a href="tab-deficiente.php" class="text-center text-base md:text-xl px-4 py-2           rounded-xl bg-yellow-600 text-white hover:bg-yellow-500 transition">
         Voltar
       </a>
